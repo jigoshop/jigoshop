849eeb6b535e4475f4d937f83435e30d7565d734
Mike Jolley <mike.jolley@me.com>
2011-06-13 10:57:05 +0100

    0.9.7.4 from git to sv svn

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,13 @@
-#! /bin/bash
-# A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
-# The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
-
 # main config
-PLUGINSLUG="jigoshop"
 CURRENTDIR=`pwd`
 MAINFILE="jigoshop.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
-GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
+GITPATH="$CURRENTDIR/" # this should be the path to your git repository (trailing slash required)
 
 # svn config
-SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/jigoshop" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNPATH="$CURRENTDIR/../jigoshop-svn/" # path to your checked out SVN repo. Trailing slash is required and don't add trunk.
 SVNUSER="jigowatt" # your svn username
-
 
 # Let's begin...
 echo ".........................................."
@@ -24,54 +17,42 @@ echo
 echo ".........................................."
 echo 
 
-# Check version in readme.txt is the same as plugin file
-NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk '{print $NF+0}'`
-echo "readme version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk '{print $NF+0}'`
-echo "$MAINFILE version: $NEWVERSION2"
-
+# check version in readme.txt is the same as plugin file
+NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk '{ print $NF}'`
+echo $NEWVERSION1;
+NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk '{ print $NF}'`
+echo $NEWVERSION2;
 if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
 
 echo "Versions match in readme.txt and PHP file. Let's proceed..."
 
+# change into the git dir and get a commit message
 cd $GITPATH
-echo -e "Enter a commit message for this new version: \c"
+echo -e "Enter a commit message describing the changes made: \c"
 read COMMITMSG
-git commit -am "$COMMITMSG"
+git commit -a -m "$COMMITMSG"
 
-echo "Tagging new version in git"
-git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
-
-echo "Pushing latest commit to origin, with tags"
+# push to origin
+echo "Push latest commit to origin"
 git push origin master
-git push origin master --tags
 
+# Export git contents to svn directory
 echo 
-echo "Creating local copy of SVN repo ..."
-svn co $SVNURL $SVNPATH
-
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
-echo "Ignoring github specific files and deployment script"
-svn propset svn:ignore "deploy.sh
-README.md
-.git
-.gitignore" "$SVNPATH/trunk/"
-
+# Change to SVN dir and commit changes
 echo "Changing directory to SVN and committing to trunk"
-cd $SVNPATH/trunk/
-# Add all new files that are not set to be ignored
-svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
+cd $SVNPATH/trunk
 svn commit --username=$SVNUSER -m "$COMMITMSG"
 
-echo "Creating new SVN tag & committing it"
+# Create a new tag and commit it :)
+echo "Creating new SVN tag"
 cd $SVNPATH
-svn copy trunk/ tags/$NEWVERSION1/
-cd $SVNPATH/tags/$NEWVERSION1
-svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
+svn copy trunk/ tags/$NEWVERSION1
+svn commit --username=$SVNUSER -m "Updating tag to $NEWVERSION1"
 
-echo "Removing temporary directory $SVNPATH"
-rm -fr $SVNPATH/
-
-echo "*** FIN ***"
+# Update the version number
+echo "Updating version number for future executions"
+cd $CURRENTDIR
+echo $NEWVERSION1 > version.txt
