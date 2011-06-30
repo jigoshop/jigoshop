6c396bfc03cdf9c6c53a114d61c3a2a52d64c8b3
Mike Jolley <mike.jolley@me.com>
2011-06-30 13:13:25 +0100

    removed add to cart templates

--- templates/product/virtual/add-to-cart.php
+++
@@ -1,9 +0,0 @@
-<?php global $_product; $availability = $_product->get_availability(); ?>
-
-<p class="stock <?php echo $availability['class'] ?>"><?php echo $availability['availability']; ?></p>
-						
-<form action="<?php echo $_product->add_to_cart_url(); ?>" class="cart" method="post">
-	<div class="quantity"><input name="quantity" value="1" size="4" title="Qty" class="input-text qty text" maxlength="12" /></div> 
-	<button type="submit" class="button-alt"><?php _e('Add to cart', 'jigoshop'); ?></button>
-	<?php jigoshop::nonce_field('add_to_cart') ?>
-</form>
