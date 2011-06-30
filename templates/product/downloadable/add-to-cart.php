97e1ec1c9a88ac1e35e68f617575111f0e174a52
Mike Jolley <mike.jolley@me.com>
2011-06-30 13:12:23 +0100

    continuing

--- templates/product/downloadable/add-to-cart.php
+++
@@ -1,8 +0,0 @@
-<?php global $_product; $availability = $_product->get_availability(); ?>
-
-<?php if ($availability['availability']) : ?><p class="stock <?php echo $availability['class'] ?>"><?php echo $availability['availability']; ?></p><?php endif; ?>
-						
-<form action="<?php echo $_product->add_to_cart_url(); ?>" class="cart" method="post">
-	<button type="submit" class="button-alt"><?php _e('Add to cart', 'jigoshop'); ?></button>
-	<?php jigoshop::nonce_field('add_to_cart') ?>
-</form>
