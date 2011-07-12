37f2c3a972ab003e74b20ee0a96d643b0e44086a
Frank Bültge <frank@bueltge.de>
2011-07-12 16:46:49 +0200

    add filter for include new pages on post process

--- templates/checkout/form.php
+++ templates/checkout/form.php
@@ -1,6 +1,8 @@
-<?php do_action('before_checkout_form'); ?>
+<?php do_action('before_checkout_form');
+// filter hook for include new pages inside the payment method
+$get_checkout_url = apply_filters( 'jigoshop_get_checkout_url', jigoshop_cart::get_checkout_url() ); ?>
 
-<form name="checkout" method="post" class="checkout" action="<?php echo jigoshop_cart::get_checkout_url(); ?>">
+<form name="checkout" method="post" class="checkout" action="<?php echo $get_checkout_url; ?>">
 	
 	<div class="col2-set" id="customer_details">
 		<div class="col-1">
