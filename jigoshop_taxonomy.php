3f585895ac5a63b2d6de07a960b6253f30128b15
Laurent Dinclaux <lox.dev@knc.nc>
2011-07-13 16:38:08 +1100

    Fixes some notice errors

--- jigoshop_taxonomy.php
+++ jigoshop_taxonomy.php
@@ -8,7 +8,7 @@ function jigoshop_post_type() {
 	
 	$shop_page_id = get_option('jigoshop_shop_page_id');
 	
-	$base_slug = $shop_page_id && get_page_uri( get_option('jigoshop_shop_page_id') ) ? get_page_uri( get_option('jigoshop_shop_page_id') ) : 'shop';
+	$base_slug = get_post($shop_page_id) && get_page_uri( $shop_page_id ) ? get_page_uri( $shop_page_id ) : 'shop';
 	
 	if (get_option('jigoshop_prepend_shop_page_to_urls')=="yes") :
 		$category_base = trailingslashit($base_slug);
