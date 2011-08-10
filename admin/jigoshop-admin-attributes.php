2ce3fee63aaa6c62c341df6b34967a36bdb78e12
Chris Balchin <chris.balchin@jigowatt.co.uk>
2011-08-10 17:01:20 +0100

    Updated License Info
    /admin main files

--- admin/jigoshop-admin-attributes.php
+++ admin/jigoshop-admin-attributes.php
@@ -4,9 +4,17 @@
  * 
  * The attributes section lets users add custom attributes to assign to products - they can also be used in the layered nav widgets.
  *
- * @author 		Jigowatt
- * @category 	Admin
- * @package 	JigoShop
+ * DISCLAIMER
+ *
+ * Do not edit or add directly to this file if you wish to upgrade Jigoshop to newer
+ * versions in the future. If you wish to customise Jigoshop core for your needs,
+ * please use our GitHub repository to publish essential changes for consideration.
+ *
+ * @package    Jigoshop
+ * @category   Admin
+ * @author     Jigowatt
+ * @copyright  Copyright (c) 2011 Jigowatt Ltd.
+ * @license    http://jigoshop.com/license/commercial-edition
  */
 
 /**
--- admin/jigoshop-admin.php
+++ admin/jigoshop-admin.php
@@ -1,15 +1,19 @@
 <?php
 /**
- * JigoShop Admin
- * 
  * Main admin file which loads all settings panels and sets up the menus.
  *
- * @author 		Jigowatt
- * @category 	Admin
- * @package 	JigoShop
+ * DISCLAIMER
+ *
+ * Do not edit or add directly to this file if you wish to upgrade Jigoshop to newer
+ * versions in the future. If you wish to customise Jigoshop core for your needs,
+ * please use our GitHub repository to publish essential changes for consideration.
+ *
+ * @package    Jigoshop
+ * @category   Admin
+ * @author     Jigowatt
+ * @copyright  Copyright (c) 2011 Jigowatt Ltd.
+ * @license    http://jigoshop.com/license/commercial-edition
  */
-
-
 
 require_once ( 'jigoshop-install.php' );
 
