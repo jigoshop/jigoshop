1a576fbf1f65ed01a74b934370efb2378e8ef543
Konrad Dzwinel <kdzwinel@gmail.com>
2011-08-24 00:23:25 +0200

    Some more code simplifications on admin order page.

--- admin/write-panels/order-data.php
+++ admin/write-panels/order-data.php
@@ -21,30 +21,9 @@ function jigoshop_order_data_meta_box($post) {
 	global $post, $wpdb, $thepostid;
 	add_action('admin_footer', 'jigoshop_meta_scripts');
 	
-	wp_nonce_field( 'jigoshop_save_data', 'jigoshop_meta_nonce' );
-	
-	$data = (array) maybe_unserialize( get_post_meta($post->ID, 'order_data', true) );
-	
-	if (!isset($data['billing_first_name'])) $data['billing_first_name'] = '';
-	if (!isset($data['billing_last_name'])) $data['billing_last_name'] = '';
-	if (!isset($data['billing_company'])) $data['billing_company'] = '';
-	if (!isset($data['billing_address_1'])) $data['billing_address_1'] = '';
-	if (!isset($data['billing_address_2'])) $data['billing_address_2'] = '';
-	if (!isset($data['billing_city'])) $data['billing_city'] = '';
-	if (!isset($data['billing_postcode'])) $data['billing_postcode'] = '';
-	if (!isset($data['billing_country'])) $data['billing_country'] = '';
-	if (!isset($data['billing_state'])) $data['billing_state'] = '';
-	if (!isset($data['billing_email'])) $data['billing_email'] = '';
-	if (!isset($data['billing_phone'])) $data['billing_phone'] = '';
-	if (!isset($data['shipping_first_name'])) $data['shipping_first_name'] = '';
-	if (!isset($data['shipping_last_name'])) $data['shipping_last_name'] = '';
-	if (!isset($data['shipping_company'])) $data['shipping_company'] = '';
-	if (!isset($data['shipping_address_1'])) $data['shipping_address_1'] = '';
-	if (!isset($data['shipping_address_2'])) $data['shipping_address_2'] = '';
-	if (!isset($data['shipping_city'])) $data['shipping_city'] = '';
-	if (!isset($data['shipping_postcode'])) $data['shipping_postcode'] = '';
-	if (!isset($data['shipping_country'])) $data['shipping_country'] = '';
-	if (!isset($data['shipping_state'])) $data['shipping_state'] = '';
+	wp_nonce_field('jigoshop_save_data', 'jigoshop_meta_nonce');
+
+    $data = (array) maybe_unserialize( get_post_meta($post->ID, 'order_data', true) );
 	
 	$data['customer_user'] = (int) get_post_meta($post->ID, 'customer_user', true);
 	
@@ -115,8 +94,10 @@ function jigoshop_order_data_meta_box($post) {
 			<textarea rows="1" cols="40" name="excerpt" tabindex="6" id="excerpt" placeholder="<?php _e('Customer\'s notes about the order', 'jigoshop'); ?>"><?php echo $post->post_excerpt; ?></textarea></p>
 		</div>
 		
-		<div id="order_customer_billing_data" class="panel jigoshop_options_panel"><?php
-        
+		<div id="order_customer_billing_data" class="panel jigoshop_options_panel">
+            <?php
+            //display billing fieds and values
+            
                 $billing_fields = array(
                     'first_name' => __('First Name', 'jigoshop'),
                     'last_name' => __('First Name', 'jigoshop'),
@@ -129,13 +110,18 @@ function jigoshop_order_data_meta_box($post) {
                     'state' => __('State/Country', 'jigoshop'),
                     'email' => __('Email Address', 'jigoshop'),
                     'phone' => __('Tel', 'jigoshop'),
-                ); 
-				
+                );
+    
                 foreach($billing_fields as $field_id => $field_desc) {
                     $field_id = 'billing_' . $field_id;
+                    $field_value = '';
+                    
+                    if(isset($data[$field_id])) {
+                        $field_value = $data[$field_id];
+                    }
                     
                     echo '<p class="form-field"><label for="'.$field_id.'">'.$field_desc.':</label>
-				<input type="text" name="'.$field_id.'" id="'.$field_id.'" value="'.$data[$field_id].'" /></p>';
+				<input type="text" name="'.$field_id.'" id="'.$field_id.'" value="'.$field_value.'" /></p>';
                 }
 				
 			?>
@@ -145,6 +131,7 @@ function jigoshop_order_data_meta_box($post) {
 		
 			<p class="form-field"><button class="button billing-same-as-shipping"><?php _e('Copy billing address to shipping address', 'jigoshop'); ?></button></p>
 			<?php
+            //display shipping fieds and values
             
                 $shipping_fields = array(
                     'first_name' => __('First Name', 'jigoshop'),
@@ -156,13 +143,18 @@ function jigoshop_order_data_meta_box($post) {
                     'postcode' => __('Postcode', 'jigoshop'),
                     'country' => __('Country', 'jigoshop'),
                     'state' => __('State/Country', 'jigoshop')
-                ); 
-				
+                );
+    
                 foreach($shipping_fields as $field_id => $field_desc) {
                     $field_id = 'shipping_' . $field_id;
+                    $field_value = '';
+                    
+                    if(isset($data[$field_id])) {
+                        $field_value = $data[$field_id];
+                    }
                     
                     echo '<p class="form-field"><label for="'.$field_id.'">'.$field_desc.':</label>
-				<input type="text" name="'.$field_id.'" id="'.$field_id.'" value="'.$data[$field_id].'" /></p>';
+				<input type="text" name="'.$field_id.'" id="'.$field_id.'" value="'.$field_value.'" /></p>';
                 }
 			?>
 		</div>
