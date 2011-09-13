a9e1f533453c0ccaf1ab9da62eba4c2b7667d0b6
Lox <lox.dev@knc.nc>
2011-09-13 22:29:51 +1100

    Raises an error on call of clone or wakeup

--- classes/abstract/singleton.php
+++ classes/abstract/singleton.php
@@ -29,4 +29,12 @@ class jigoshop_singleton extends jigoshop_class {
     
     }
     
+	public function __clone() {
+        trigger_error('Clone is not allowed.', E_USER_ERROR);
+    }
+
+    public function __wakeup() {
+        trigger_error('Unserializing is not allowed.', E_USER_ERROR);
+    }
+    
 }
