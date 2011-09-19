a763f96f761407e8b8397e8e65ff1028f7695b29
Laurent Dinclaux <lox.dev@knc.nc>
2011-09-19 11:02:46 +1100

    Twenty eleven theme fixes

--- jigoshop_template_functions.php
+++ jigoshop_template_functions.php
@@ -54,7 +54,7 @@ if (!function_exists('jigoshop_output_content_wrapper')) {
 }
 if (!function_exists('jigoshop_output_content_wrapper_end')) {
 	function jigoshop_output_content_wrapper_end() {	
-		if(  get_option('template') === 'twentyeleven' ) echo  '</section></div>';
+		if(  get_option('template') === 'twentyeleven' ) echo  '</div></section>';
 		else echo '</div></div>';	
 	}
 }
@@ -764,10 +764,14 @@ if (!function_exists('jigoshop_breadcrumb')) {
 	}
 }
 
-
+/**
+ * Hook to remove the 'singular' class, for the twenty eleven theme, to properly display the sidebar
+ * 
+ * @param array $classes
+ */
 function jigoshop_body_classes ($classes) {
 	
-	if( ! is_singular('product') ) return $classes;
+	if(  ! is_post_type_archive('product') && ! is_product() && ! is_product_category() && ! is_product_tag() ) return $classes;
 	
 	$key = array_search('singular', $classes);
 	if ( $key !== false ) unset($classes[$key]);
