e74e7d04e7ee780033627047540e74121b09e42c
Jeff Parsons <jeffrey.allen.parsons@gmail.com>
2011-10-13 22:47:39 -0300

    introduce jigoshop_base and jigoshop_singleton abstract classes

---
+++ classes/abstract/jigoshop_singleton.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Abstract Singleton Class: Ensure that there can be only one instance of Class. Provide a global access point to that instance.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add directly to this file if you wish to upgrade Jigoshop to newer
+ * versions in the future. If you wish to customise Jigoshop core for your needs,
+ * please use our GitHub repository to publish essential changes for consideration.
+ *
+ * @package    Jigoshop
+ * @category   Core
+ * @author     Jigowatt
+ * @copyright  Copyright (c) 2011 Jigowatt Ltd.
+ * @license    http://jigoshop.com/license/commercial-edition
+ */
+
+require_once 'jigoshop_base.class.php';
+
+
+abstract class jigoshop_singleton extends jigoshop_base_class {
+
+    private static $instance = array();
+    
+    
+    protected function __construct() { }
+	
+	
+    public static function instance() {
+		
+    	$class = get_called_class();
+		
+    	if ( isset( self::$instance[$class] ) ) return self::$instance[$class];
+    	
+    	$args = func_get_args();
+    	
+    	self::$instance[$class] = new $class( $args );
+    	
+    	return self::$instance[$class];
+    
+    }
+    
+    
+	public function __clone() {
+        trigger_error( "Cloning Singleton's is not allowed.", E_USER_ERROR );
+    }
+	
+	
+    public function __wakeup() {
+        trigger_error( "Unserializing Singleton's is not allowed.", E_USER_ERROR );
+    }
+    
+}
