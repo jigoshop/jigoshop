fcb48ec564ae552161f81819c1b2ddcb4eeee383
Robert Rhoades <opsrob@gmail.com>
2011-11-23 01:12:33 +0000

    Added login widget

--- jigoshop_widgets.php
+++ jigoshop_widgets.php
@@ -26,5 +26,6 @@ function jigoshop_register_widgets() {
 	register_widget('Jigoshop_Widget_Layered_Nav');
 	register_widget('Jigoshop_Widget_Price_Filter');
 	register_widget('Jigoshop_Widget_Product_Search');
+	register_widget('Jigoshop_Widget_User_Login');
 }
 add_action('widgets_init', 'jigoshop_register_widgets');
