da2bd650dbd25b074edfb9e4a1dd527db586f069
Julian Jöris <j.joeris@inpsyde.com>
2011-11-28 13:53:46 +0100

    clean

--- classes/jigoshop_shipping.class.php
+++ classes/jigoshop_shipping.class.php
@@ -140,6 +140,7 @@ class jigoshop_shipping extends jigoshop_singleton {
 //			if ( ! isset( $_available_methods[$chosen_method] )) :
 				//$chosen_method = $_cheapest_method;
 //			endif;
+
 			//bugfix 
 			$chosen_method = $chosen_method-id;
 			if ( $chosen_method ) :
