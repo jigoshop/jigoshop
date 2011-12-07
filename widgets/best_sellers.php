d08cee51d44282a9fe4fb60532a9bf37f45b6461
Rob Rhoades <robert.rhoades@jigowatt.co.uk>
2011-12-07 15:55:33 +0000

    Code formatting tweaks

--- widgets/best_sellers.php
+++ widgets/best_sellers.php
@@ -8,12 +8,12 @@
  * versions in the future. If you wish to customise Jigoshop core for your needs,
  * please use our GitHub repository to publish essential changes for consideration.
  *
- * @package    Jigoshop
- * @category   Widgets
- * @author     Jigowatt
- * @since	   1.0
- * @copyright  Copyright (c) 2011 Jigowatt Ltd.
- * @license    http://jigoshop.com/license/commercial-edition
+ * @package 	Jigoshop
+ * @category 	Widgets
+ * @author 	Jigowatt
+ * @since 	1.0
+ * @copyright 	Copyright (c) 2011 Jigowatt Ltd.
+ * @license 	http://jigoshop.com/license/commercial-edition
  */
 
 class Jigoshop_Widget_Best_Sellers extends WP_Widget {
@@ -26,16 +26,16 @@ class Jigoshop_Widget_Best_Sellers extends WP_Widget {
 	 */
 	public function __construct() {
 		$options = array(
-			'classname'		=> 'widget_best_sellers',
-			'description'	=> __( "Lists the best selling products", 'jigoshop')
+			'classname'	=> 'widget_best_sellers',
+			'description'	=> __( 'Lists the best selling products', 'jigoshop')
 		);
 		
-		parent::__construct('recent-products', __('Jigoshop: Best Sellers', 'jigoshop'), $options);
+		parent::__construct( 'recent-products', __( 'Jigoshop: Best Sellers', 'jigoshop' ), $options );
 
 		// Flush cache after every save
-		add_action( 'save_post', array(&$this, 'flush_widget_cache') );
-		add_action( 'deleted_post', array(&$this, 'flush_widget_cache') );
-		add_action( 'switch_theme', array(&$this, 'flush_widget_cache') );
+		add_action( 'save_post', array( &$this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( &$this, 'flush_widget_cache' ) );
+		add_action( 'switch_theme', array( &$this, 'flush_widget_cache' ) );
 	}
 
 	/**
@@ -47,58 +47,61 @@ class Jigoshop_Widget_Best_Sellers extends WP_Widget {
 	 * @param	array	sidebar arguments
 	 * @param	array	instance
 	 */
-	public function widget($args, $instance) {
+	public function widget( $args, $instance ) {
 	
 		// Get the most recent products from the cache
-		$cache = wp_cache_get('widget_best_sellers', 'widget');
+		$cache = wp_cache_get( 'widget_best_sellers', 'widget' );
 		
 		// If no entry exists use array
-		if ( ! is_array($cache) ) {
+		if ( ! is_array( $cache ) ) {
 			$cache = array();
 		}
 
 		// If cached get from the cache
-		if ( isset($cache[$args['widget_id']]) ) {
+		if ( isset( $cache[$args['widget_id']] ) ) {
 			echo $cache[$args['widget_id']];
 			return false;
 		}
 
 		// Start buffering
 		ob_start();
-		extract($args);
+		extract( $args );
 
 		// Set the widget title
-		$title = apply_filters('widget_title', 
-			($instance['title']) ? $instance['title'] : __('Best Sellers', 'jigoshop'), 
-			$instance, $this->id_base);
+		$title = apply_filters(
+			'widget_title', 
+			( $instance['title'] ) ? $instance['title'] : __( 'Best Sellers', 'jigoshop' ), 
+			$instance,
+			$this->id_base
+		);
 		
 		// Set number of products to fetch
-		if ( ! $number = $instance['number'] ) {
-			$number = 10;
+		if ( ! $number = absint( $instance['number'] ) ) {
+			$number = 5;
 		}
 
 		// Set up query
-    	$query_args = array(
-    		'showposts'		=> $number,
-    		'post_type'		=> 'product',
-    		'post_status'	=> 'publish',
-    		'meta_key'		=> '_js_total_sales',
-    		'orderby'		=> 'meta_value',
-    		'nopaging'		=> 0,
-    		'meta_query'	=> array(
-    			array(
-    				'key'		=> 'visibility',
-    				'value'		=> array('catalog', 'visible'),
-    				'compare'	=> 'IN',
-    			),
-    		)
-    	);
+	    	$query_args = array(
+	    		'showposts'	=> $number,
+	    		'post_type'	=> 'product',
+	    		'post_status'	=> 'publish',
+	    		'meta_key'	=> '_js_total_sales',
+	    		'orderby'	=> 'meta_value',
+	    		'nopaging'	=> true,
+	    		'meta_query'	=> array(
+	    			array(
+	    				'key'		=> 'visibility',
+	    				'value'		=> array('catalog', 'visible'),
+	    				'compare'	=> 'IN',
+	    			),
+	    		)
+	    	);
 
 		// Run the query
-		$q = new WP_Query($query_args);
+		$q = new WP_Query( $query_args );
 		
 		// If there are products
-		if($q->have_posts()) {
+		if( $q->have_posts() ) {
 		
 			// Print the widget wrapper & title
 			echo $before_widget;
@@ -111,12 +114,17 @@ class Jigoshop_Widget_Best_Sellers extends WP_Widget {
 			while($q->have_posts()) : $q->the_post();  
 				
 				// Get new jigoshop_product instance
-				$_product = new jigoshop_product(get_the_ID());
+				$_product = new jigoshop_product( get_the_ID() );
 			
 				echo '<li>';
 					// Print the product image & title with a link to the permalink
-					echo '<a href="'.get_permalink().'" title="'.esc_attr(get_the_title()).'">';
-					echo (has_post_thumbnail()) ? the_post_thumbnail('shop_tiny') : jigoshop_get_image_placeholder('shop_tiny');
+					echo '<a href="'.get_permalink().'" title="'.esc_attr( get_the_title() ).'">';
+					
+					// Print the product image
+					echo ( has_post_thumbnail() ) 
+						? the_post_thumbnail( 'shop_tiny' )
+						: jigoshop_get_image_placeholder( 'shop_tiny' );
+
 					echo '<span class="js_widget_product_title">' . get_the_title() . '</span>';
 					echo '</a>';
 					
@@ -136,7 +144,7 @@ class Jigoshop_Widget_Best_Sellers extends WP_Widget {
 		
 		// Flush output buffer and save to cache
 		$cache[$args['widget_id']] = ob_get_flush();
-		wp_cache_set('widget_best_sellers', $cache, 'widget');
+		wp_cache_set( 'widget_best_sellers', $cache, 'widget' );
 	}
 
 	/**
@@ -153,16 +161,16 @@ class Jigoshop_Widget_Best_Sellers extends WP_Widget {
 		$instance = $old_instance;
 		
 		// Save the new values
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['number'] = abs($new_instance['number']);
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['number'] = absint( $new_instance['number'] );
 
 		// Flush the cache
 		$this->flush_widget_cache();
 
 		// Remove the cache entry from the options array
 		$alloptions = wp_cache_get( 'alloptions', 'options' );
-		if ( isset($alloptions['widget_best_sellers']) ) {
-			delete_option('widget_best_sellers');
+		if ( isset( $alloptions['widget_best_sellers'] ) ) {
+			delete_option( 'widget_best_sellers' );
 		}
 
 		return $instance;
@@ -174,7 +182,7 @@ class Jigoshop_Widget_Best_Sellers extends WP_Widget {
 	 * Flushes the cached output
 	 */
 	public function flush_widget_cache() {
-		wp_cache_delete('widget_best_sellers', 'widget');
+		wp_cache_delete( 'widget_best_sellers', 'widget' );
 	}
 
 	/**
@@ -187,22 +195,22 @@ class Jigoshop_Widget_Best_Sellers extends WP_Widget {
 	public function form( $instance ) {
 	
 		// Get instance data
-		$title = isset($instance['title']) ? esc_attr($instance['title']) : null;
-		
-		$number = apply_filters('jigoshop_widget_featured_default_number', 5, $instance, $this->id_base);
-		$number = isset($instance['number']) ? abs($instance['number']) : $number;
-		
+		$title 		= isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : null;
+		$number 	= isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+
 		// Widget Title
-		echo '<p>';
-		echo '<label for="' . $this->get_field_id('title') . '">' . _e('Title:', 'jigoshop') . '</label>';
-		echo '<input class="widefat" id="' . $this->get_field_id('title') . '" name="' . $this->get_field_name('title') . '" type="text" value="'. $title .'" />';
-		echo '</p>';
-		
+		echo "
+		<p>
+			<label for='{$this->get_field_id( 'title' )}'>" . __( 'Title:', 'jigoshop' ) . "</label>
+			<input class='widefat' id='{$this->get_field_id( 'title' )}' name='{$this->get_field_name( 'title' )}' type='text' value='{$title}' />
+		</p>";
+
 		// Number of posts to fetch
-		echo '<p>';
-		echo '<label for="' . $this->get_field_id('number') . '">' . _e('Number of products to show:', 'jigoshop') . '</label>';
-		echo '<input id="' . $this->get_field_id('number') . '" name="' . $this->get_field_name('number') . '" type="text" value="' . $number . '" size="3" />';
-		echo '</p>';
+		echo "
+		<p>
+			<label for='{$this->get_field_id( 'number' )}'>" . __( 'Number of products to show:', 'jigoshop' ) . "</label>
+			<input id='{$this->get_field_id( 'number' )}' name='{$this->get_field_name( 'number' )}' type='number' min='1' value='{$number}' />
+		</p>";
 
 	}
 	
