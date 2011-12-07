c7a3dfdd5a843abb387751764645d11b4060bded
Julian Jöris <j.joeris@inpsyde.com>
2011-12-07 12:50:56 +0100

    little bugfix on bugfix ;)

--- templates/checkout/review_order.php
+++ templates/checkout/review_order.php
@@ -143,8 +143,8 @@
 				if ($available_gateways) : 
 					// Chosen Method
 					if (sizeof($available_gateways)) {
-						if( isset( $_POST[ 'payment_method' ] ) && isset( $available_gateways [ $_POST['payment_method'] ] ) ) {
-							$available_gateways [ $_POST[ 'payment_method' ] ]->set_current();
+						if( isset( $_POST[ 'payment_method' ] ) && isset( $available_gateways[ $_POST['payment_method'] ] ) ) {
+							$available_gateways[ $_POST[ 'payment_method' ] ]->set_current();
 						} else {
 							current($available_gateways)->set_current();
 						}	
