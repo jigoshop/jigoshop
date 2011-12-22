91398cd1f8c68e32607a4e11b8e504c5607622e8
Matt Gates <info@mgates.me>
2011-12-22 16:48:24 -0500

    Change 'parent post' to 'parent product'

--- admin/write-panels/product-data.php
+++ admin/write-panels/product-data.php
@@ -72,7 +72,7 @@ function jigoshop_product_data_box() {
 			$posts_in = (array) get_objects_in_term( get_term_by( 'slug', 'grouped', 'product_type' )->term_id, 'product_type' );
 			$posts_in = array_unique($posts_in);
 			
-			$field = array( 'id' => 'parent_id', 'label' => __('Parent post', 'jigoshop') );
+			$field = array( 'id' => 'parent_id', 'label' => __('Parent Product', 'jigoshop') );
 			echo '<p class="form-field parent_id_field"><label for="'.$field['id'].'">'.$field['label'].'</label><select id="'.$field['id'].'" name="'.$field['id'].'"><option value="">'.__('Choose a grouped product&hellip;', 'jigoshop').'</option>';
 
 			if (sizeof($posts_in)>0) :
