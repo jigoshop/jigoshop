a786d802db7f96d1f1017134033da5f163009e2a
Robert Rhoades <opsrob@gmail.com>
2011-12-31 03:32:52 +0000

    Grouped set all options

--- admin/write-panels/product-types/variable.php
+++ admin/write-panels/product-types/variable.php
@@ -180,13 +180,15 @@ class jigoshop_product_meta_variable extends jigoshop_product_meta
 				<select name="variation_actions">
 					<option value="default"><?php _e('Bulk Actions', 'jigoshop') ?></option>
 					<option value="remove_all"><?php _e('Remove All Variations', 'jigoshop') ?></option>
-					<option value="set_all_regular_prices"><?php _e('Set all Prices', 'jigoshop') ?></option>
-					<option value="set_all_sale_prices"><?php _e('Set all Sale Prices', 'jigoshop') ?></option>
-					<option value="set_all_stock"><?php _e('Set all Stock', 'jigoshop') ?></option>
-					<option value="set_all_weight"><?php _e('Set all Weight', 'jigoshop') ?></option>
-					<option value="set_all_width"><?php _e('Set all Width', 'jigoshop') ?></option>
-					<option value="set_all_length"><?php _e('Set all Length', 'jigoshop') ?></option>
-					<option value="set_all_height"><?php _e('Set all Height', 'jigoshop') ?></option>
+					<optgroup label="<?php _e('Set All', 'jigoshop') ?>:">
+						<option value="set_all_regular_prices"><?php _e('Regular Prices', 'jigoshop') ?></option>
+						<option value="set_all_sale_prices"><?php _e('Sale Prices', 'jigoshop') ?></option>
+						<option value="set_all_stock"><?php _e('Stock', 'jigoshop') ?></option>
+						<option value="set_all_weight"><?php _e('Weight', 'jigoshop') ?></option>
+						<option value="set_all_width"><?php _e('Width', 'jigoshop') ?></option>
+						<option value="set_all_length"><?php _e('Length', 'jigoshop') ?></option>
+						<option value="set_all_height"><?php _e('Height', 'jigoshop') ?></option>
+					</optgroup>
 				</select>
 				<input id="do_actions" type="submit" class="button-secondary" value="Apply">
 
