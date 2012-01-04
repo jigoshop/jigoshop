a1611f2e46969e97f2c210849d3316d0ee5a03d0
unknown <mandbmontean@hotmail.com>
2012-01-03 23:38:04 -0500

    Remove upstream/dev tag
    Forgot to remove the upstream/dev tag from code when doing manual merge.

--- templates/checkout/review_order.php
+++ templates/checkout/review_order.php
@@ -238,80 +238,77 @@
 								<td>' . $values['quantity'] . '</td>
 								<td>' . jigoshop_price($_product->get_price_excluding_tax() * $values['quantity'], array('ex_tax_label' => 1)) . '</td>
 							</tr>';
-                    endif;
-                endforeach;
-            endif;
-            ?>
-        </tbody>
-    </table>
+					endif;
+				endforeach; 
+			endif;
+			?>
+		</tbody>
+	</table>
+	
+	<div id="payment">
+		<?php if (jigoshop_cart::needs_payment()) : ?>
+		<ul class="payment_methods methods">
+			<?php 
+				$available_gateways = jigoshop_payment_gateways::get_available_payment_gateways();
+				if ($available_gateways) : 
+					// Chosen Method
+					if (sizeof($available_gateways)) {
+						if( isset( $_POST[ 'payment_method' ] ) && isset( $available_gateways[ $_POST['payment_method'] ] ) ) {
+							$available_gateways[ $_POST[ 'payment_method' ] ]->set_current();
+						} else {
+							current($available_gateways)->set_current();
+						}	
+					}
+					foreach ($available_gateways as $gateway ) :
+						?>
+						<li>
+						<input type="radio" id="payment_method_<?php echo $gateway->id; ?>" class="input-radio" name="payment_method" value="<?php echo $gateway->id; ?>" <?php if ($gateway->chosen) echo 'checked="checked"'; ?> />
+						<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->title; ?> <?php echo apply_filters('gateway_icon', $gateway->icon(), $gateway->id); ?></label> 
+							<?php
+								if ($gateway->has_fields || $gateway->description) : 
+									echo '<div class="payment_box payment_method_'.$gateway->id.'" style="display:none;">';
+									$gateway->payment_fields();
+									echo '</div>';
+								endif;
+							?>
+						</li>
+						<?php
+					endforeach;
+				else :
+				
+					if ( !jigoshop_customer::get_country() ) :
+						echo '<p>'.__('Please fill in your details above to see available payment methods.', 'jigoshop').'</p>';
+					else :
+						echo '<p>'.__('Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'jigoshop').'</p>';
+					endif;
+					
+				endif;
+			?>
+		</ul>
+		<?php endif; ?>
 
-    <div id="payment">
-        <?php if (jigoshop_cart::needs_payment()) : ?>
-            <ul class="payment_methods methods">
-                <?php
-                $available_gateways = jigoshop_payment_gateways::get_available_payment_gateways();
-                if ($available_gateways) :
-                    // Chosen Method
-                    if (sizeof($available_gateways)) {
-                        if (isset($_POST['payment_method']) && isset($available_gateways[$_POST['payment_method']])) {
-                            $available_gateways[$_POST['payment_method']]->set_current();
-                        } else {
-                            current($available_gateways)->set_current();
-                        }
-                    }
-                    foreach ($available_gateways as $gateway) :
-                        ?>
-                        <li>
-                            <input type="radio" id="payment_method_<?php echo $gateway->id; ?>" class="input-radio" name="payment_method" value="<?php echo $gateway->id; ?>" <?php if ($gateway->chosen)
-                echo 'checked="checked"'; ?> />
-                            <label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->title; ?> <?php echo apply_filters('gateway_icon', $gateway->icon(), $gateway->id); ?></label> 
-                            <?php
-                            if ($gateway->has_fields || $gateway->description) :
-                                echo '<div class="payment_box payment_method_' . $gateway->id . '" style="display:none;">';
-                                $gateway->payment_fields();
-                                echo '</div>';
-                            endif;
-                            ?>
-                        </li>
-                        <?php
-                    endforeach;
-                else :
+		<div class="form-row">
+		
+			<noscript><?php _e('Since your browser does not support JavaScript, or it is disabled, please ensure you click the <em>Update Totals</em> button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'jigoshop'); ?><br/><input type="submit" class="button-alt" name="update_totals" value="<?php _e('Update totals', 'jigoshop'); ?>" /></noscript>
+		
+			<?php jigoshop::nonce_field('process_checkout')?>
+			
+			<?php do_action( 'jigoshop_review_order_before_submit' ); ?>
+			
+			<?php if (get_option('jigoshop_terms_page_id')>0) : ?>
+			<p class="form-row terms">
+				<label for="terms" class="checkbox"><?php _e('I accept the', 'jigoshop'); ?> <a href="<?php echo get_permalink(get_option('jigoshop_terms_page_id')); ?>" target="_blank"><?php _e('terms &amp; conditions', 'jigoshop'); ?></a></label>
+				<input type="checkbox" class="input-checkbox" name="terms" <?php if (isset($_POST['terms'])) echo 'checked="checked"'; ?> id="terms" />
+			</p>
+			<?php endif; ?>
+			
+			<?php $order_button_text = apply_filters( 'jigoshop_order_button_text', __( 'Place order', 'jigoshop') ); ?>
+			<input type="submit" class="button-alt" name="place_order" id="place_order" value="<?php echo $order_button_text; ?>" />
+			
+			<?php do_action( 'jigoshop_review_order_after_submit' ); ?>
+			
+		</div>
 
-                    if (!jigoshop_customer::get_country()) :
-                        echo '<p>' . __('Please fill in your details above to see available payment methods.', 'jigoshop') . '</p>';
-                    else :
-                        echo '<p>' . __('Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'jigoshop') . '</p>';
-                    endif;
-
-                endif;
-                ?>
-            </ul>
-        <?php endif; ?>
-
-        <div class="form-row">
-
-            <noscript><?php _e('Since your browser does not support JavaScript, or it is disabled, please ensure you click the <em>Update Totals</em> button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'jigoshop'); ?><br/><input type="submit" class="button-alt" name="update_totals" value="<?php _e('Update totals', 'jigoshop'); ?>" /></noscript>
-
-            <?php jigoshop::nonce_field('process_checkout') ?>
-
-            <?php $order_button_text = apply_filters('jigoshop_order_button_text', __('Place order', 'jigoshop')); ?>
-
-            <input type="submit" class="button-alt" name="place_order" id="place_order" value="<?php echo $order_button_text; ?>" />
-
-            <?php do_action('jigoshop_review_order_before_submit'); ?>
-
-            <?php if (get_option('jigoshop_terms_page_id') > 0) : ?>
-                <p class="form-row terms">
-                    <label for="terms" class="checkbox"><?php _e('I accept the', 'jigoshop'); ?> <a href="<?php echo get_permalink(get_option('jigoshop_terms_page_id')); ?>" target="_blank"><?php _e('terms &amp; conditions', 'jigoshop'); ?></a></label>
-                    <input type="checkbox" class="input-checkbox" name="terms" <?php if (isset($_POST['terms']))
-                    echo 'checked="checked"'; ?> id="terms" />
-                </p>
-            <?php endif; ?>
-
-            <?php do_action('jigoshop_review_order_after_submit'); ?>
-
-        </div>
-
-    </div>
+	</div>
 
 </div>
