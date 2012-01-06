75c62b1602a5688458d87c5da48c6fda2b30aae6
Rob Rhoades <robert.rhoades@jigowatt.co.uk>
2012-01-06 16:26:45 +0000

    Converted spaces to tabs for indentation

--- classes/jigoshop_product_variation.class.php
+++ classes/jigoshop_product_variation.class.php
@@ -30,7 +30,7 @@ class jigoshop_product_variation extends jigoshop_product {
 	public function __construct( $ID ) {
 
 		// Setup the product
-        $parent_id = wp_get_post_parent_id( $ID );
+		$parent_id = wp_get_post_parent_id( $ID );
 		parent::__construct( $parent_id );
 
 		// Get the meta & for each meta item overwrite with the variations ID
@@ -45,66 +45,66 @@ class jigoshop_product_variation extends jigoshop_product {
 		$this->variation_id = $ID;
 		$this->variation_data = maybe_unserialize( $this->meta['variation_data'][0] );
 		parent::__construct( $ID );
-        
-        // Restore the parent ID
-        $this->ID = $parent_id;
-        $this->id = $parent_id;
+
+		// Restore the parent ID
+		$this->ID = $parent_id;
+		$this->id = $parent_id;
 
 		return $this;
 	}
-    
-    /**
-     * Get variation ID
-     * 
-     * @return  int
-     */
-    public function get_variation_id() {
-        return (int) $this->variation_id;
-    }
-    
-    /**
-     * Get variation attribute values
-     * 
-     * @uses    for get_available_attributes_variations()
-     * @return  two dimensional array array of attributes and their values for this variation
-     */
-    public function get_variation_attributes() {
-        return $this->variation_data; // @todo: This returns blank if its set to catch all, how would we deal with that?
-    }
 
-    /**
-     * Modifies the stock levels for variations
-     *
-     * @param   int   Amount to modify
-     * @return  int
-     */
-    public function modify_stock( $by ) {
+	/**
+	 * Get variation ID
+	 * 
+	 * @return  int
+	 */
+	public function get_variation_id() {
+		return (int) $this->variation_id;
+	}
 
-        // Only do this if we're updating
-        if ( ! $this->managing_stock() )
-            return false;
-        
-        // +- = minus
-        $this->stock = $this->stock + $by;
-        $amount_sold = $this->stock_sold + $by;
-        
-        // Update & return the new value
-        update_post_meta( $this->variation_id, 'stock', $this->stock );
-        update_post_meta( $this->variation_id, 'stock_sold', $amount_sold );
-        return $this->stock;
-    }
-    
-    /**
-     * Update values of variation attributes using given values
-     * TODO: Why do we need this?
-     *
-     * @param   array $data array of attributes and values
-     */
-    function set_variation_attributes(array $data) {
-        foreach($this->variation_data as $attribute=>$value) {
-            if(isset($data[$attribute])) {
-                $this->variation_data[$attribute] = $data[$attribute];
-            }
-        }
-    }
+	/**
+	 * Get variation attribute values
+	 * 
+	 * @uses    for get_available_attributes_variations()
+	 * @return  two dimensional array array of attributes and their values for this variation
+	 */
+	public function get_variation_attributes() {
+		return $this->variation_data; // @todo: This returns blank if its set to catch all, how would we deal with that?
+	}
+
+	/**
+	 * Modifies the stock levels for variations
+	 *
+	 * @param   int   Amount to modify
+	 * @return  int
+	 */
+	public function modify_stock( $by ) {
+
+		// Only do this if we're updating
+		if ( ! $this->managing_stock() )
+			return false;
+
+		// +- = minus
+		$this->stock = $this->stock + $by;
+		$amount_sold = $this->stock_sold + $by;
+
+		// Update & return the new value
+		update_post_meta( $this->variation_id, 'stock', $this->stock );
+		update_post_meta( $this->variation_id, 'stock_sold', $amount_sold );
+		return $this->stock;
+	}
+
+	/**
+	 * Update values of variation attributes using given values
+	 * TODO: Why do we need this?
+	 *
+	 * @param   array $data array of attributes and values
+	 */
+	function set_variation_attributes(array $data) {
+		foreach($this->variation_data as $attribute=>$value) {
+			if(isset($data[$attribute])) {
+				$this->variation_data[$attribute] = $data[$attribute];
+			}
+		}
+	}
 }
