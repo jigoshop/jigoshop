4621346c13d2cb8d430d6945b57a9d13d0aa429d
Dimitry Zolotaryov <dimitry@webit.ca>
2012-01-27 16:53:53 -0500

    Only displaying the coupon code field in the cart when there are coupon codes in the system

--- shortcodes/cart.php
+++ shortcodes/cart.php
@@ -20,10 +20,13 @@ function get_jigoshop_cart( $atts ) {
 
 function jigoshop_cart( $atts ) {
 
-	$errors = array();
+	$errors  = array();
+
+    $jigoshop_coupons = new jigoshop_coupons();
+    $coupons          = $jigoshop_coupons->get_coupons();
 
 	// Process Discount Codes
-	if (isset($_POST['apply_coupon']) && $_POST['apply_coupon'] && jigoshop::verify_nonce('cart')) :
+	if (!empty($coupons) && isset($_POST['apply_coupon']) && $_POST['apply_coupon'] && jigoshop::verify_nonce('cart')) :
 
 		$coupon_code = stripslashes(trim($_POST['coupon_code']));
 		jigoshop_cart::add_discount($coupon_code);
@@ -137,27 +140,29 @@ function jigoshop_cart( $atts ) {
 			?>
 		</tbody>
 		<tfoot>
-			<tr>
-				<td colspan="6" class="actions">
-					<div class="coupon">
-						<label for="coupon_code"><?php _e('Coupon', 'jigoshop'); ?>:</label> <input name="coupon_code" class="input-text" id="coupon_code" value="" />
-						<input type="submit" class="button" name="apply_coupon" value="<?php _e('Apply Coupon', 'jigoshop'); ?>" />
-					</div>
-					<?php jigoshop::nonce_field('cart') ?>
-					<input type="submit" class="button" name="update_cart" value="<?php _e('Update Shopping Cart', 'jigoshop'); ?>" /> <a href="<?php echo jigoshop_cart::get_checkout_url(); ?>" class="checkout-button button-alt"><?php _e('Proceed to Checkout &rarr;', 'jigoshop'); ?></a>
-				</td>
-			</tr>
-			<?php if ( count( jigoshop_cart::$applied_coupons ) ) : ?>
-				<tr>
-					<td colspan="6" class="applied-coupons">
-						<div>
-							<span class="applied-coupons-label"><?php _e('Applied Discount Coupons: ','jigoshop'); ?></span>
-							<span class="applied-coupons-values"><?php echo implode( ',', jigoshop_cart::$applied_coupons ); ?></span>
-						</div>
-					</td>
-				</tr>
-			<?php 
-			endif;
+            <?php if ( !empty( $coupons ) ) : ?>
+                <tr>
+                    <td colspan="6" class="actions">
+                        <div class="coupon">
+                            <label for="coupon_code"><?php _e('Coupon', 'jigoshop'); ?>:</label> <input name="coupon_code" class="input-text" id="coupon_code" value="" />
+                            <input type="submit" class="button" name="apply_coupon" value="<?php _e('Apply Coupon', 'jigoshop'); ?>" />
+                        </div>
+                        <?php jigoshop::nonce_field('cart') ?>
+                        <input type="submit" class="button" name="update_cart" value="<?php _e('Update Shopping Cart', 'jigoshop'); ?>" /> <a href="<?php echo jigoshop_cart::get_checkout_url(); ?>" class="checkout-button button-alt"><?php _e('Proceed to Checkout &rarr;', 'jigoshop'); ?></a>
+                    </td>
+                </tr>
+                <?php if ( count( jigoshop_cart::$applied_coupons ) ) : ?>
+                    <tr>
+                        <td colspan="6" class="applied-coupons">
+                            <div>
+                                <span class="applied-coupons-label"><?php _e('Applied Discount Coupons: ','jigoshop'); ?></span>
+                                <span class="applied-coupons-values"><?php echo implode( ',', jigoshop_cart::$applied_coupons ); ?></span>
+                            </div>
+                        </td>
+                    </tr>
+                <?php 
+                endif;
+            endif;
 		
 			do_action( 'jigoshop_shop_table_cart_foot' ); 
 			?>
