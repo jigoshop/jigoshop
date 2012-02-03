7937773862a3caaed8a89efe59a2f9b9ef5b9de3
Jeff Parsons <jeffrey.allen.parsons@gmail.com>
2012-02-03 01:24:11 -0400

    make us first plugin loaded - fix #532

--- jigoshop.php
+++ jigoshop.php
@@ -923,3 +923,26 @@ function jigoshop_import_start() {
 
 }
 add_action('import_start', 'jigoshop_import_start');
+
+/**
+ * Support for Jigoshop being the very first plugin loaded by WordPress
+ *
+ * Will only happen once a user has 'activated' a (any) plugin.
+ *
+ * Ensures our sessions are run first.
+ * Runs last on the 'activated_plugin' hook due to a low priority.
+ *
+ **/
+function jigoshop_plugin_loads_first() {
+	// ensure path to this file is via main wp plugin path
+	$wp_path_to_this_file = preg_replace( '/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__ );
+	$this_plugin = plugin_basename( trim( $wp_path_to_this_file ));
+	$active_plugins = get_option( 'active_plugins' );
+	$this_plugin_key = array_search( $this_plugin, $active_plugins );
+	if ( $this_plugin_key ) { // if it's 0 it's the first plugin already, no need to continue
+		array_splice( $active_plugins, $this_plugin_key, 1 );
+		array_unshift( $active_plugins, $this_plugin );
+		update_option( 'active_plugins', $active_plugins );
+	}
+}
+add_action( 'activated_plugin', 'jigoshop_plugin_loads_first', 999 );
