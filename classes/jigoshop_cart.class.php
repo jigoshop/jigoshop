69b94f49409402469a67b710ff96000d7b8aca58
Matt <info@mgates.me>
2012-02-06 21:46:00 -0500

    Merge /jigoshop/dev/ pull requests (ob_flush not included)

--- classes/jigoshop_cart.class.php
+++ classes/jigoshop_cart.class.php
@@ -626,12 +626,12 @@ class jigoshop_cart extends jigoshop_singleton {
         $return = '';
 
         if (jigoshop_cart::get_tax_amount($tax_class, false) > 0) :
-            $return = self::$tax->get_tax_class_for_display($tax_class) . ' (' . (float) jigoshop_cart::get_tax_rate($tax_class) . '%):';
+            $return = self::$tax->get_tax_class_for_display($tax_class) . ' (' . (float) jigoshop_cart::get_tax_rate($tax_class) . '%): ';
 
             // only show estimated tag when customer is on the cart page and no shipping calculator is enabled to be able to change
             // country
             if (!jigoshop_shipping::show_shipping_calculator() && !( defined('JIGOSHOP_CHECKOUT') && JIGOSHOP_CHECKOUT )) :
-                $return .= '<small>' . sprintf(__('estimated for %s', 'jigoshop'), jigoshop_countries::estimated_for_prefix() . jigoshop_countries::$countries[ jigoshop_countries::get_base_country() ] ) . '</small>';
+                $return .= '<small>' . sprintf(__('estimated for %s', 'jigoshop'), jigoshop_countries::estimated_for_prefix() . __(jigoshop_countries::$countries[jigoshop_countries::get_base_country()], 'jigoshop')) . '</small>';
             endif; 
         endif;
         
