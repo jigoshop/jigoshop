c6121c12bdf80aaf21de90440cc33c59e105a62f
Jeff Parsons <jeffrey.allen.parsons@gmail.com>
2012-02-19 16:24:10 -0400

    replace 1.0 image settings

--- classes/jigoshop_options.class.php
+++ classes/jigoshop_options.class.php
@@ -640,39 +640,82 @@ class Jigoshop_Options extends Jigoshop_Singleton {
 			array( 'name' => __('Image Options', 'jigoshop'), 'type' => 'title','desc' => __('Large variations from the defaults could require CSS modifications in your Theme.','jigoshop') ),
 		
 			array(
-				'name' 		=> __('Tiny Images','jigoshop'),
-				'tip'		=> '',
-				'desc' 		=> __('The small image used in the Cart, Checkout, Orders and Widgets','jigoshop'),
-				'id' 		=> 'jigoshop_shop_tiny',
-				'css' 		=> '',
-				'type' 		=> 'image_size',
+				'name' 		=> __('Tiny Image Width','jigoshop'),
+				'desc' 		=> __('Default = 36px','jigoshop'),
+				'tip' 		=> __('Set the width of the small image used in the Cart, Checkout, Orders and Widgets.','jigoshop'),
+				'id' 		=> 'jigoshop_shop_tiny_w',
+				'css' 		=> 'min-width:200px;',
+				'type' 		=> 'text',
 				'std' 		=> 36
 			),
 		
 			array(
-				'name' 		=> __('Thumbnail Images','jigoshop'),
-				'desc' 		=> __('The thumbnail image for Single Product page extra images.','jigoshop'),
-				'id' 		=> 'jigoshop_shop_thumbnail',
-				'css' 		=> '',
-				'type' 		=> 'image_size',
+				'name' 		=> __('Tiny Image Height','jigoshop'),
+				'desc' 		=> __('Default = 36px','jigoshop'),
+				'tip' 		=> __('Set the height of the small image used in the Cart, Checkout, Orders and Widgets.','jigoshop'),
+				'id' 		=> 'jigoshop_shop_tiny_h',
+				'css' 		=> 'min-width:200px;',
+				'type' 		=> 'text',
+				'std' 		=> 36
+			),
+		
+			array(
+				'name' 		=> __('Thumbnail Image Width','jigoshop'),
+				'desc' 		=> __('Default = 90px','jigoshop'),
+				'tip' 		=> __('Set the width of the thumbnail image for Single Product page extra images.','jigoshop'),
+				'id' 		=> 'jigoshop_shop_thumbnail_w',
+				'css' 		=> 'min-width:200px;',
+				'type' 		=> 'text',
 				'std' 		=> 90
 			),
 		
 			array(
-				'name' => __( 'Catalog Images', 'jigoshop' ),
-				'desc' 		=> __('The catalog image for Shop, Categories, Tags, and Related Products.', 'jigoshop'),
-				'id' 		=> 'jigoshop_shop_small',
-				'css' 		=> '',
-				'type' 		=> 'image_size',
+				'name' 		=> __('Thumbnail Image Height','jigoshop'),
+				'desc' 		=> __('Default = 90px','jigoshop'),
+				'tip' 		=> __('Set the height of the thumbnail image for Single Product page extra images.','jigoshop'),
+				'id' 		=> 'jigoshop_shop_thumbnail_h',
+				'css' 		=> 'min-width:200px;',
+				'type' 		=> 'text',
+				'std' 		=> 90
+			),
+		
+			array(
+				'name' 		=> __('Catalog Image Width','jigoshop'),
+				'desc' 		=> __('Default = 150px','jigoshop'),
+				'tip' 		=> __('Set the width of the catalog image for Shop, Categories, Tags, and Related Products.','jigoshop'),
+				'id' 		=> 'jigoshop_shop_small_w',
+				'css' 		=> 'min-width:200px;',
+				'type' 		=> 'text',
 				'std' 		=> 150
 			),
 		
 			array(
-				'name' 		=> __('Large Images','jigoshop'),
-				'desc' 		=> __('Single Product page\'s large or Featured image.','jigoshop'),
-				'id' 		=> 'jigoshop_shop_large',
-				'css' 		=> '',
-				'type' 		=> 'image_size',
+				'name' 		=> __('Catalog Image Height','jigoshop'),
+				'desc' 		=> __('Default = 150px','jigoshop'),
+				'tip' 		=> __('Set the height of the catalog image for Shop, Categories, Tags, and Related Products.','jigoshop'),
+				'id' 		=> 'jigoshop_shop_small_h',
+				'css' 		=> 'min-width:200px;',
+				'type' 		=> 'text',
+				'std' 		=> 150
+			),
+		
+			array(
+				'name' 		=> __('Large Image Width','jigoshop'),
+				'desc' 		=> __('Default = 300px','jigoshop'),
+				'tip' 		=> __('Set the width of the Single Product page large or Featured image.','jigoshop'),
+				'id' 		=> 'jigoshop_shop_large_w',
+				'css' 		=> 'min-width:200px;',
+				'type' 		=> 'text',
+				'std' 		=> 300
+			),
+		
+			array(
+				'name' 		=> __('Large Image Height','jigoshop'),
+				'desc' 		=> __('Default = 300px','jigoshop'),
+				'tip' 		=> __('Set the height of the Single Product page large or Featured image.','jigoshop'),
+				'id' 		=> 'jigoshop_shop_large_h',
+				'css' 		=> 'min-width:200px;',
+				'type' 		=> 'text',
 				'std' 		=> 300
 			),
 		
