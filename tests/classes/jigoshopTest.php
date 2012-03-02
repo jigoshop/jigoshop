518a9a5dc3a93127aaadc72a1527db7b12915634
Rob Rhoades <robert.rhoades@jigowatt.co.uk>
2012-03-02 16:21:29 +0000

    Added some more tests

--- tests/classes/jigoshopTest.php
+++ tests/classes/jigoshopTest.php
@@ -10,16 +10,16 @@ class jigoshopTest extends WP_UnitTestCase {
         parent::setUp();
     }
 
-    public function testPlugin_url() {
+    public function test_plugin_url() {
         $this->assertEquals('http://example.org/wp-content/plugins/jigoshop', jigoshop::plugin_url());
     }
 
-    public function testPlugin_path() {
+    public function test_plugin_path() {
         $plugin_root = dirname(dirname(dirname(__FILE__)));
         $this->assertEquals( $plugin_root, jigoshop::plugin_path() );
     }
 
-    public function testGet_var() {
+    public function test_get_var() {
         $vars = array(
             'shop_small_w'     => '150',
             'shop_small_h'     => '150', 
@@ -36,12 +36,7 @@ class jigoshopTest extends WP_UnitTestCase {
         }
     }
 
-    /**
-     * Ssl on
-     *
-     * @return void
-     */
-    public function testForce_ssl()
+    public function test_force_ssl()
     {
         // Set up the test
         $unsecure_url = 'http://google.com';
@@ -62,7 +57,7 @@ class jigoshopTest extends WP_UnitTestCase {
         $this->assertEquals( 'http://google.com', $url );
     }
 
-    public function testAdd_error() {
+    public function test_add_error() {
 
         // perform the change
         jigoshop::add_error('Hello World');
@@ -71,7 +66,16 @@ class jigoshopTest extends WP_UnitTestCase {
 
     }
 
-    public function testClear_messages() {
+    public function test_add_message() {
+
+        // perform the change
+        jigoshop::add_message('Hello World');
+
+        $this->assertEquals( TRUE, in_array('Hello World', jigoshop::$messages));
+
+    }
+
+    public function test_clear_messages() {
         jigoshop::add_error('Hello World');
 
         // then clear it
@@ -80,5 +84,21 @@ class jigoshopTest extends WP_UnitTestCase {
         // did it work?
         $this->assertEmpty(jigoshop::$errors, 'jigoshop::$errors still has something in it');
         $this->assertEmpty(jigoshop::$messages, 'jigoshop::$messages still has something in it');
+    }
+
+    public function test_show_messages() {
+        // set up
+        jigoshop::add_error( 'Hello World' );
+
+        $this->expectOutputString('<div class="jigoshop_error">Hello World</div>');
+        jigoshop::show_messages();
+
+        error_log( print_r(jigoshop::show_messages(), true) );
+        exit;
+
+        jigoshop::add_message( 'Foo Bar' );
+
+        $this->expectOutputString('<div class="jigoshop_error">Foo Bar</div>');
+        jigoshop::show_messages();
     }
 }
