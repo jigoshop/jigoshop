646828ba9c7cb5f5963d00c848c0f4bc1e871acb
Michael Montean <mmontean@gmail.com>
2012-03-24 16:09:10 -0400

    Fix undefined index

--- classes/jigoshop_shipping.class.php
+++ classes/jigoshop_shipping.class.php
@@ -176,7 +176,7 @@ class jigoshop_shipping extends jigoshop_singleton {
 
             if (sizeof($_available_methods) > 0) :
 
-                if (!empty( jigoshop_session::instance()->selected_rate_id )) :
+                if (!empty( jigoshop_session::instance()->selected_rate_id ) && !empty($chosen_method)) :
 
                     //make sure all methods are re-calculated since prices have been reset. Otherwise the other shipping
                     //method prices will show free
@@ -187,7 +187,7 @@ class jigoshop_shipping extends jigoshop_singleton {
 
                     // select chosen method
                     if ($_available_methods[$chosen_method] &&
-                            (!($available_methods[$chosen_method] instanceof jigoshop_calculable_shipping)
+                            (!($_available_methods[$chosen_method] instanceof jigoshop_calculable_shipping)
                             || ($_available_methods[$chosen_method] instanceof jigoshop_calculable_shipping && !$_available_methods[$chosen_method]->has_error()))) :
                         $chosen_method = $_available_methods[$chosen_method]->id;
 
