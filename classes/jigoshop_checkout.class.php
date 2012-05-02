7d817ff8207588cbfa316bc340a22dc97581c51a
Matt <info@mgates.me>
2012-05-02 12:41:32 -0400

    @mindbuster, revisits 29bfa6cab45906e10da45b385114cfedd931c5b4 .
    is_in_stock() always returned true when manage_stock is 'no', so that last if statement wasn't even being ran since it's checking for a 'false' value.
    
    Previous if block:
    
    Is manage stock enabled?
     Yes: Is it not in stock or does it not have enough stock?
     No: Is it not in stock? -- It will never be in stock because manage stock is disabled xD
    
    Current if block:
    
    Is manage stock enabled? And is a product out of stock or not have enough stock?

--- classes/jigoshop_checkout.class.php
+++ classes/jigoshop_checkout.class.php
@@ -605,19 +605,12 @@ class jigoshop_checkout extends jigoshop_singleton {
 					 	), $values);
 
 					 	// Check stock levels
-					 	if ($_product->managing_stock()) :
-							if (!$_product->is_in_stock() || !$_product->has_enough_stock( $values['quantity'] )) :
-								$errormsg = (get_option('jigoshop_show_stock') == 'yes') ? (sprintf(__('Sorry, we do not have enough "%s" in stock to fulfill your order.  We have %d available at this time. Please edit your cart and try again. We apologize for any inconvenience caused.', 'jigoshop'), $_product->get_title(), $_product->get_stock() )) : (sprintf(__('Sorry, we do not have enough "%s" in stock to fulfill your order. Please edit your cart and try again. We apologize for any inconvenience caused.', 'jigoshop'), $_product->get_title() ));
-								jigoshop::add_error($errormsg);
-		                		break;
-							endif;
-						else :
-							if (!$_product->is_in_stock()) :
-								$errormsg = (get_option('jigoshop_show_stock') == 'yes') ? (sprintf(__('Sorry, we do not have enough "%s" in stock to fulfill your order.  We have %d available at this time. Please edit your cart and try again. We apologize for any inconvenience caused.', 'jigoshop'), $_product->get_title(), $_product->get_stock() )) : (sprintf(__('Sorry, we do not have enough "%s" in stock to fulfill your order. Please edit your cart and try again. We apologize for any inconvenience caused.', 'jigoshop'), $_product->get_title() ));
-								jigoshop::add_error($errormsg);
-		                		break;
-							endif;
-
+					 	if ( $_product->managing_stock() && (!$_product->is_in_stock() || !$_product->has_enough_stock($values['quantity']) ) ) :
+							$errormsg = (get_option('jigoshop_show_stock') == 'yes')
+							? (sprintf(__('Sorry, we do not have enough "%s" in stock to fulfill your order.  We have %d available at this time. Please edit your cart and try again. We apologize for any inconvenience caused.', 'jigoshop'), $_product->get_title(), $_product->get_stock() ))
+							: (sprintf(__('Sorry, we do not have enough "%s" in stock to fulfill your order. Please edit your cart and try again. We apologize for any inconvenience caused.', 'jigoshop'), $_product->get_title() ));
+							jigoshop::add_error($errormsg);
+							break;
 						endif;
 
 					endforeach;
