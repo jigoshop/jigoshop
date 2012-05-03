503c331f9ddc2b370887353cba74d7ad6c2ee765
Michael Montean <mmontean@gmail.com>
2012-05-03 01:27:32 -0400

    Fix payment gateway selection on radio buttons in checkout

--- templates/checkout/review_order.php
+++ templates/checkout/review_order.php
@@ -128,7 +128,7 @@
                                     if( isset( $_POST[ 'payment_method' ] ) && isset( $available_gateways[ $_POST['payment_method'] ] ) ) :
                                         $available_gateways[ $_POST[ 'payment_method' ] ]->set_current();
                                     else :
-                                        current($available_gateways)->set_current();
+                                        $gateway->set_current();
                                     endif;
                                 endif;
                                 $gateway_set = true;
