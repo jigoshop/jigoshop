de248de546564ea383f9dc92e6c6ec39ba823c16
Jeff Parsons <jeffrey.allen.parsons@gmail.com>
2012-06-28 12:25:29 -0300

    setup initial Coupon columns

--- admin/jigoshop-admin-post-types.php
+++ admin/jigoshop-admin-post-types.php
@@ -669,3 +669,59 @@ function jigoshop_post_updated_messages($messages) {
     endif;
     return $messages;
 }
+
+/**
+ * Columns for Coupons page
+ **/
+add_filter('manage_edit-shop_coupon_columns', 'jigoshop_edit_coupon_columns');
+
+function jigoshop_edit_coupon_columns($columns){
+	
+	$columns = array();
+	
+//	$columns["cb"] 			= "<input type=\"checkbox\" />";
+	$columns["title"] 		= __("Code", 'jigoshop');
+	$columns["type"] 		= __("Coupon type", 'jigoshop');
+	$columns["amount"] 		= __("Coupon amount", 'jigoshop');
+	$columns["usage_limit"] = __("Usage limit", 'jigoshop');
+	$columns["usage_count"] = __("Usage count", 'jigoshop');
+	$columns["expiry_date"] = __("Expiry date", 'jigoshop');
+
+	return $columns;
+}
+
+
+/**
+ * Custom Columns for Coupons page
+ **/
+add_action('manage_shop_coupon_posts_custom_column', 'jigoshop_custom_coupon_columns', 2);
+
+function jigoshop_custom_coupon_columns($column) {
+	global $post;
+	
+	$type 			= get_post_meta($post->ID, 'discount_type', true);
+	$amount 		= get_post_meta($post->ID, 'coupon_amount', true);
+	$individual_use = get_post_meta($post->ID, 'individual_use', true);
+	$product_ids 	= (get_post_meta($post->ID, 'product_ids', true)) ? explode(',', get_post_meta($post->ID, 'product_ids', true)) : array();
+	$usage_limit 	= get_post_meta($post->ID, 'usage_limit', true);
+	$usage_count 	= (int) get_post_meta($post->ID, 'usage_count', true);
+	$expiry_date 	= get_post_meta($post->ID, 'expiry_date', true);
+
+	switch ($column) {
+		case "type" :
+			echo $type;			
+		break;
+		case "amount" :
+			echo $amount;
+		break;
+		case "usage_limit" :
+			if ($usage_limit) echo $usage_limit; else echo '&ndash;';
+		break;
+		case "usage_count" :
+			echo $usage_count;
+		break;
+		case "expiry_date" :
+			if ($expiry_date) echo date('F j, Y', strtotime($expiry_date)); else echo '&ndash;';
+		break;
+	}
+}
