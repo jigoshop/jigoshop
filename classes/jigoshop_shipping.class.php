079e40705991dec97a555e8658406486557b0a13
Jeff Parsons <jeffrey.allen.parsons@gmail.com>
2012-07-10 11:25:33 -0300

    repair free shipping coupon

--- classes/jigoshop_shipping.class.php
+++ classes/jigoshop_shipping.class.php
@@ -87,7 +87,7 @@ class jigoshop_shipping extends Jigoshop_Singleton {
 			if (jigoshop_cart::$applied_coupons)
 				foreach (jigoshop_cart::$applied_coupons as $code) :
 					if ( $coupon = jigoshop_coupons::get_coupon($code) ) :
-						if ($coupon['free_shipping'] == 'yes')
+						if ( isset($coupon['free_shipping']) )
 							$free_shipping = 1;
 					endif;
 				endforeach;
