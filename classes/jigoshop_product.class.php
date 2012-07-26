4bec4c8ef7da41927b0e76907ee814711c2bec5d
Sławomir Chrobak <slawomir.chrobak@gmail.com>
2012-07-26 20:45:32 +0200

    Added hooks for required for new plugin
    I have written discounts plugin and I need those hooks to work with it.

--- classes/jigoshop_product.class.php
+++ classes/jigoshop_product.class.php
@@ -720,9 +720,9 @@ class jigoshop_product {
 		else if ( $this->sale_price )
 			 return $this->sale_price;
 
-		else return $this->regular_price;
+        else return apply_filters('jigoshop_product_get_regular_price', $this->regular_price, $this->ID);
 
-	}
+    }
 
 	/**
 	 * Returns the products sale value, either with or without a percentage
@@ -818,8 +818,8 @@ class jigoshop_product {
 		if ( $this->regular_price == '' )
 			$html = __( 'Price Not Announced', 'jigoshop' );
 
-		return $html;
-	}
+        return apply_filters('jigoshop_product_get_price_html', $html, $this, $this->regular_price);
+    }
 
 	/**
 	 * Returns the upsell product ids
