ce0862eeeb9b561ab8b8fefdc6c62053f55ef5c3
Alex Sancho <alex@alexsancho.name>
2012-12-23 17:06:52 +0100

    bring back media upload for pre 3.5

--- admin/jigoshop-admin-taxonomies.php
+++ admin/jigoshop-admin-taxonomies.php
@@ -18,69 +18,28 @@
 /**
  * Category thumbnails
  */
-add_action('product_cat_add_form_fields' , 'jigoshop_add_category_thumbnail_field');
-add_action('product_cat_edit_form_fields', 'jigoshop_edit_category_thumbnail_field', 10,2);
+add_action( 'product_cat_add_form_fields' , 'jigoshop_add_category_thumbnail_field' );
+add_action( 'product_cat_edit_form_fields', 'jigoshop_edit_category_thumbnail_field', 10, 2 );
 
 function jigoshop_add_category_thumbnail_field() {
 	$image = jigoshop::assets_url().'/assets/images/placeholder.png';
 	?>
 	<div class="form-field">
-		<label><?php _e('Thumbnail', 'jigoshop'); ?></label>
+		<label><?php _e( 'Thumbnail', 'jigoshop' ); ?></label>
 		<div id="product_cat_thumbnail" style="float:left;margin-right:10px;"><img src="<?php echo $image; ?>" width="60px" height="60px" /></div>
 		<div style="line-height:60px;">
 			<input type="hidden" id="product_cat_thumbnail_id" name="product_cat_thumbnail_id" />
-			<button type="submit" class="upload_image_button button"><?php _e('Upload/Add image', 'jigoshop'); ?></button>
-			<button type="submit" class="remove_image_button button"><?php _e('Remove image', 'jigoshop'); ?></button>
+			<button type="submit" class="upload_image_button button"><?php _e( 'Upload/Add image', 'jigoshop' ); ?></button>
+			<button type="submit" class="remove_image_button button"><?php _e( 'Remove image', 'jigoshop' ); ?></button>
 		</div>
 		<script type="text/javascript">
 			var wpActiveEditor;
 
-			jQuery('.upload_image_button').click(function() {
+			jQuery('.upload_image_button').on('click', function(event) {
+				event.preventDefault();
 
-			    var send_attachment_bkp = wp.media.editor.send.attachment;
-
-			    wp.media.editor.send.attachment = function(props, attachment) {
-					jQuery('#product_cat_thumbnail_id').val(attachment.id);
-					jQuery('#product_cat_thumbnail img').attr('src', attachment.url);
-
-			        wp.media.editor.send.attachment = send_attachment_bkp;
-			    }
-
-			    wp.media.editor.open();
-
-			    return false;
-			});
-
-			jQuery('.remove_image_button').live('click', function(){
-				jQuery('#product_cat_thumbnail img').attr('src', '<?php echo $image; ?>');
-				jQuery('#product_cat_thumbnail_id').val('');
-				return false;
-			});
-
-		</script>
-		<div class="clear"></div>
-	</div>
-	<?php
-}
-
-function jigoshop_edit_category_thumbnail_field( $term, $taxonomy ) {
-	$image = jigoshop_product_cat_image($term->term_id);
-	?>
-	<tr class="form-field">
-		<th scope="row" valign="top"><label><?php _e('Thumbnail', 'jigoshop'); ?></label></th>
-		<td>
-			<div id="product_cat_thumbnail" style="float:left;margin-right:10px;"><img src="<?php echo $image['image']; ?>" width="60px" height="60px" /></div>
-			<div style="line-height:60px;">
-				<input type="hidden" id="product_cat_thumbnail_id" name="product_cat_thumbnail_id" value="<?php echo $image['thumb_id']; ?>" />
-				<button type="submit" class="upload_image_button button"><?php _e('Upload/Add image', 'jigoshop'); ?></button>
-				<button type="submit" class="remove_image_button button"><?php _e('Remove image', 'jigoshop'); ?></button>
-			</div>
-			<script type="text/javascript">
-				var wpActiveEditor;
-
-				jQuery('.upload_image_button').click(function() {
-
-				    var send_attachment_bkp = wp.media.editor.send.attachment;
+				if(typeof wp !== 'undefined') {
+					var send_attachment_bkp = wp.media.editor.send.attachment;
 
 				    wp.media.editor.send.attachment = function(props, attachment) {
 						jQuery('#product_cat_thumbnail_id').val(attachment.id);
@@ -90,14 +49,69 @@ function jigoshop_edit_category_thumbnail_field( $term, $taxonomy ) {
 				    }
 
 				    wp.media.editor.open();
+				} else {
+					var post_id = 0;
+					window.send_to_editor = window.send_to_termmeta;
 
-				    return false;
+					tb_show('', 'media-upload.php?post_id=' + post_id + '&amp;type=image&amp;TB_iframe=true');
+				}
+			});
+
+			jQuery('.remove_image_button').on('click', function(event) {
+				event.prevendDefault();
+
+				jQuery('#product_cat_thumbnail img').attr('src', '<?php echo $image['image']; ?>');
+				jQuery('#product_cat_thumbnail_id').val('');
+			});
+
+		</script>
+		<div class="clear"></div>
+	</div>
+	<?php
+}
+
+function jigoshop_edit_category_thumbnail_field( $term, $taxonomy ) {
+	$image = jigoshop_product_cat_image( $term->term_id );
+	?>
+	<tr class="form-field">
+		<th scope="row" valign="top"><label><?php _e( 'Thumbnail', 'jigoshop' ); ?></label></th>
+		<td>
+			<div id="product_cat_thumbnail" style="float:left;margin-right:10px;"><img src="<?php echo $image['image']; ?>" width="60px" height="60px" /></div>
+			<div style="line-height:60px;">
+				<input type="hidden" id="product_cat_thumbnail_id" name="product_cat_thumbnail_id" value="<?php echo $image['thumb_id']; ?>" />
+				<button type="submit" class="upload_image_button button"><?php _e( 'Upload/Add image', 'jigoshop' ); ?></button>
+				<button type="submit" class="remove_image_button button"><?php _e( 'Remove image', 'jigoshop' ); ?></button>
+			</div>
+			<script type="text/javascript">
+				var wpActiveEditor;
+
+				jQuery('.upload_image_button').on('click', function(event) {
+					event.preventDefault();
+
+					if(typeof wp !== 'undefined') {
+						var send_attachment_bkp = wp.media.editor.send.attachment;
+
+					    wp.media.editor.send.attachment = function(props, attachment) {
+							jQuery('#product_cat_thumbnail_id').val(attachment.id);
+							jQuery('#product_cat_thumbnail img').attr('src', attachment.url);
+
+					        wp.media.editor.send.attachment = send_attachment_bkp;
+					    };
+
+					    wp.media.editor.open();
+					} else {
+						var post_id = 0;
+						window.send_to_editor = window.send_to_termmeta;
+
+						tb_show('', 'media-upload.php?post_id=' + post_id + '&amp;type=image&amp;TB_iframe=true');
+					}
 				});
 
-				jQuery('.remove_image_button').live('click', function(){
-					jQuery('#product_cat_thumbnail img').attr('src', '<?php echo jigoshop::assets_url().'/assets/images/placeholder.png'; ?>');
+				jQuery('.remove_image_button').on('click', function(event) {
+					event.prevendDefault();
+
+					jQuery('#product_cat_thumbnail img').attr('src', '<?php echo $image['image']; ?>');
 					jQuery('#product_cat_thumbnail_id').val('');
-					return false;
 				});
 
 			</script>
@@ -107,12 +121,12 @@ function jigoshop_edit_category_thumbnail_field( $term, $taxonomy ) {
 	<?php
 }
 
-add_action('created_term', 'jigoshop_category_thumbnail_field_save', 10,3);
-add_action('edit_term'   , 'jigoshop_category_thumbnail_field_save', 10,3);
+add_action( 'created_term', 'jigoshop_category_thumbnail_field_save', 10, 3 );
+add_action( 'edit_term'   , 'jigoshop_category_thumbnail_field_save', 10, 3 );
 
 function jigoshop_category_thumbnail_field_save( $term_id, $tt_id, $taxonomy ) {
 
-	if (!isset($_POST['product_cat_thumbnail_id']))
+	if ( !isset( $_POST['product_cat_thumbnail_id'] ) )
 		return false;
 
 	update_metadata( 'jigoshop_term', $term_id, 'thumbnail_id', $_POST['product_cat_thumbnail_id'] );
@@ -120,19 +134,19 @@ function jigoshop_category_thumbnail_field_save( $term_id, $tt_id, $taxonomy ) {
 }
 
 /**
-* Thumbnail column for categories
-*/
-add_filter("manage_edit-product_cat_columns", 'jigoshop_product_cat_columns');
-add_filter("manage_product_cat_custom_column", 'jigoshop_product_cat_column', 10, 3);
+ * Thumbnail column for categories
+ */
+add_filter( 'manage_edit-product_cat_columns', 'jigoshop_product_cat_columns' );
+add_filter( 'manage_product_cat_custom_column', 'jigoshop_product_cat_column', 10, 3 );
 
 function jigoshop_product_cat_columns( $columns ) {
 
 	$new_columns = array(
 		'cb'    => $columns['cb'],
-		'thumb' => null
+		'thumb' => null,
 	);
 
-	unset($columns['cb']);
+	unset( $columns['cb'] );
 	$columns = array_merge( $new_columns, $columns );
 
 	return $columns;
@@ -141,10 +155,10 @@ function jigoshop_product_cat_columns( $columns ) {
 
 function jigoshop_product_cat_column( $columns, $column, $id ) {
 
-	if ($column != 'thumb')
+	if ( $column != 'thumb' )
 		return false;
 
-	$image = jigoshop_product_cat_image($id);
+	$image    = jigoshop_product_cat_image( $id );
 	$columns .= '<a class="row-title" href="'.get_edit_term_link( $id, 'product_cat', 'product' ).'">';
 	$columns .= '<img src="'.$image['image'].'" alt="Thumbnail" class="wp-post-image" height="32" width="32" />';
 	$columns .= '</a>';
--- assets/js/variable.js
+++ assets/js/variable.js
@@ -1,219 +1,261 @@
 (function($) {
-	// @todo: varmeta is a temporary name.. this should be a global jigoshop var
+    // @todo: varmeta is a temporary name.. this should be a global jigoshop var
 
-	$(function() {
+    $(function() {
 
-		// This is a unique ID for add_variation
-		var ID = 0;
+        // This is a unique ID for add_variation
+        var ID = 0;
 
-		// Unbind the standard postbox click event
-		$('.jigoshop_variation.postbox h3').unbind('click.postboxes');
+        // Unbind the standard postbox click event
+        $('.jigoshop_variation.postbox h3').unbind('click.postboxes');
 
-		// Set up the events bound to the root variable product options so events don't bubble to the document
-		$('#variable_product_options')
-		.on('change', '.product_type', function(e) {
+        // Set up the events bound to the root variable product options so events don't bubble to the document
+        $('#variable_product_options')
+        .on('change', '.product_type', function(e) {
 
-			var $this	= $(this);
-		 	      panel	= '.'+$this.val();
-		 	      $root	= $this.parent().parent().parent()
+            var $this   = $(this),
+                  panel = '.'+$this.val(),
+                  $root = $this.parent().parent().parent();
 
-			// Hide all the panels first then show the relevant one
-			// TODO: can this be improved anyway?
-			$root.find('.options').hide();
-			$root.find(panel).show();
-		})
+            // Hide all the panels first then show the relevant one
+            // TODO: can this be improved anyway?
+            $root.find('.options').hide();
+            $root.find(panel).show();
+        })
 
-		.on('click', '.postbox h3', function(e) {
+        .on('click', '.postbox h3', function(e) {
 
-			// the jquery event can still be triggered by other child elements, so we need to be explicit
-			if ( e.target.tagName.toLowerCase() != 'h3' && !$(this).hasClass('handlediv'))
-				return false;
+            // the jquery event can still be triggered by other child elements, so we need to be explicit
+            if ( e.target.tagName.toLowerCase() != 'h3' && !$(this).hasClass('handlediv'))
+                return false;
 
-			$(this).parent().toggleClass('closed');
-		})
+            $(this).parent().toggleClass('closed');
+        })
 
-		// @todo: this should be an ID
-		.on('click', '.add_variation', function(e) {
+        // @todo: this should be an ID
+        .on('click', '.add_variation', function(e) {
 
-			// Disable default action
-			e.preventDefault();
+            // Disable default action
+            e.preventDefault();
 
-			// Remove the demo if it exists
-			$('.demo.variation').remove();
+            // Remove the demo if it exists
+            $('.demo.variation').remove();
 
-			// Replace the ID with a unique ID
-			html = varmeta.actions.create.panel.replace(/__ID__/gi, ID++ +'_new');
+            // Replace the ID with a unique ID
+            html = varmeta.actions.create.panel.replace(/__ID__/gi, ID++ +'_new');
 
-			// Append a new variation panel
-			$(html).removeClass('closed').hide().prependTo('.jigoshop_variations').slideDown( 150 );
-		})
+            // Append a new variation panel
+            $(html).removeClass('closed').hide().prependTo('.jigoshop_variations').slideDown( 150 );
+        })
 
-		// @todo: this should be an ID
-		.on('click', '.remove_variation', function(e) {
+        // @todo: this should be an ID
+        .on('click', '.remove_variation', function(e) {
 
-			// Disable default action
-			e.preventDefault();
+            // Disable default action
+            e.preventDefault();
 
-			// Only remove if the user is sure
-			if ( ! confirm(varmeta.actions.remove.confirm) )
-				return false;
+            // Only remove if the user is sure
+            if ( ! confirm(varmeta.actions.remove.confirm) )
+                return false;
 
-			remove_variation($(this).parent());
-		})
+            remove_variation($(this).parent());
+        })
 
-		.on('click', '.upload_image_button', function(e) {
+        .on('click', '.upload_image_button', function(e) {
 
-			// Disable default action
-			e.preventDefault();
+            // Disable default action
+            e.preventDefault();
 
-			// Set up variables
-			var $this		= $(this);
-				$img		= $this.find('img');
-				$imgID		= $this.find('input');
-				$parent		= $this.parent();
-				post_id		= $this.attr('rel');
-				formfield	= $('.upload_image_id').attr('name');
+            // Set up variables
+            var $this       = $(this);
+                $img        = $this.find('img');
+                $imgID      = $this.find('input');
+                $parent     = $this.parent();
+                post_id     = $this.attr('rel');
+                formfield   = $('.upload_image_id').attr('name');
 
-			// Remove the image
-			if ( $this.is('.remove') ) {
+            // Remove the image
+            if ( $this.is('.remove') ) {
 
-				// Set the hidden input value as null
-				$imgID.val(null);
+                // Set the hidden input value as null
+                $imgID.val(null);
 
-				// Replace the image with the placeholder
-				$img.attr('src', varmeta.assets_url+'/assets/images/placeholder.png');
-				$this.removeClass('remove');
-			}
-			else {
-			    var send_attachment_bkp = wp.media.editor.send.attachment;
+                // Replace the image with the placeholder
+                $img.attr('src', varmeta.assets_url+'/assets/images/placeholder.png');
+                $this.removeClass('remove');
+            }
+            else {
+                if(typeof wp !== 'undefined') {
+                    var send_attachment_bkp = wp.media.editor.send.attachment;
 
-			    wp.media.editor.send.attachment = function(props, attachment) {
-					// Set the vhidden input value with the thumb ID
-					$imgID.val(attachment.id);
+                    wp.media.editor.send.attachment = function(props, attachment) {
+                        // Set the vhidden input value with the thumb ID
+                        $imgID.val(attachment.id);
 
-					// Replace the image with a preview of the image
-					$('img', $parent).attr('src', attachment.url);
-					$this.addClass('remove');
+                        // Replace the image with a preview of the image
+                        $('img', $parent).attr('src', attachment.url);
+                        $this.addClass('remove');
 
-			        wp.media.editor.send.attachment = send_attachment_bkp;
-			    }
+                        wp.media.editor.send.attachment = send_attachment_bkp;
+                    };
 
-			    wp.media.editor.open();
+                    wp.media.editor.open();
+                } else {
+                    window.send_to_editor = function( html ) {
+                        // Set up variables
+                        var $img        = $(html).find('img');
+                            imgsrc      = $img.attr('src');
+                            imgclass        = $img.attr('class');
+                            imgid       = parseInt(imgclass.replace(/\D/g, ''), 10);
 
-			    return false;
-			}
-		})
+                        // Set the vhidden input value with the thumb ID
+                        $imgID.val(imgid);
 
-		.on('click', '.upload_file_button', function(e) {
+                        // Replace the image with a preview of the image
+                        $('img', $parent).attr('src', imgsrc);
+                        $this.addClass('remove');
 
-			// Disable default action
-			e.preventDefault();
+                        // Hide thickbox
+                        tb_remove();
+                    };
 
-			// Set up variables
-			var $this   = $(this);
-			    $file   = $this.prev();
-			    post_id = $this.parents('.jigoshop_variation').attr('rel');
+                    // @todo: Why do we need this? -Rob
+                    // formfield = $('.upload_image_id', $parent).attr('name');
 
-		    var send_attachment_bkp = wp.media.editor.send.attachment;
+                    // Show thickbox
+                    tb_show('', 'media-upload.php?post_id'+post_id+'&type=image&TB_iframe=true');
+                }
 
-		    wp.media.editor.send.attachment = function(props, attachment) {
+                return false;
+            }
+        })
 
-				// Attach the file URI to the relevant
-				$file.val( attachment.url );
+        .on('click', '.upload_file_button', function(e) {
 
-		        wp.media.editor.send.attachment = send_attachment_bkp;
-		    }
+            // Disable default action
+            e.preventDefault();
 
-		    wp.media.editor.open();
+            // Set up variables
+            var $this   = $(this);
+                $file   = $this.prev();
+                post_id = $this.parents('.jigoshop_variation').attr('rel');
 
-		    return false;
-		})
+            if(typeof wp !== 'undefined') {
+                var send_attachment_bkp = wp.media.editor.send.attachment;
 
-		.on('click', '#do_actions', function(e) {
+                wp.media.editor.send.attachment = function(props, attachment) {
 
-			// Disable default action
-			e.preventDefault();
+                    // Attach the file URI to the relevant
+                    $file.val( attachment.url );
 
-			if ( ! $('.jigoshop_variation').size() )
-				return alert( varmeta.i18n.variations_required );
+                    wp.media.editor.send.attachment = send_attachment_bkp;
+                };
 
-			// Cache variables
-			var $this = $(this);
-			$this.trigger( $this.prev().val() );
-		})
+                wp.media.editor.open();
+            } else {
+                window.send_to_editor = function(html) {
+                    // Attach the file URI to the relevant
+                    $file.val( $(html).attr('href') );
 
-		.on({
-			remove_all: function(e) {
-				if( ! confirm( varmeta.i18n.remove_all ) )
-					return false;
+                    // Hide thickbox
+                    tb_remove();
+                };
 
-				$('.jigoshop_variation').each( function() {
-					remove_variation( $(this) );
-				});
-			},
-			set_all_regular_prices: function(e) {
-				value = prompt( varmeta.i18n.set_regular_price );
-				$(' input[name*="regular_price"] ').val( value );
-			},
-			set_all_sale_prices: function(e) {
-				value = prompt( varmeta.i18n.set_sale_price );
-				$(' input[name*="sale_price"] ').val( value );
-			},
-			set_all_stock: function(e) {
-				value = prompt( varmeta.i18n.set_stock );
-				$(' input[name*="stock"] ').val( value );
-			},
-			set_all_weight: function(e) {
-				value = prompt( varmeta.i18n.set_weight );
-				$(' input[name*="weight"] ').val( value );
-			},
-			set_all_width: function(e) {
-				value = prompt( varmeta.i18n.set_width );
-				$(' input[name*="width"] ').val( value );
-			},
-			set_all_length: function(e) {
-				value = prompt( varmeta.i18n.set_length );
-				$(' input[name*="length"] ').val( value );
-			},
-			set_all_height: function(e) {
-				value = prompt( varmeta.i18n.set_height );
-				$(' input[name*="height"] ').val( value );
-			}
-		});
+                // @todo: Why do we need this? -Rob
+                // formfield = $(parent).attr('name');
 
-	});
+                // Show thickbox
+                tb_show('', 'media-upload.php?post_id=' + post_id + '&type=downloadable_product&from=jigoshop_variation&TB_iframe=true');
+            }
 
-	function remove_variation( $panel ) {
+            return false;
+        })
 
-		// Set up the variables
-		var variation	= $panel.attr('rel');
-			data 		= {
-				action: varmeta.actions.remove.action,
-				variation_id: variation,
-				security: varmeta.actions.remove.nonce,
-			};
+        .on('click', '#do_actions', function(e) {
 
-		// If the variation already exists
-		if ( variation.indexOf('_new') < 0 ) {
+            // Disable default action
+            e.preventDefault();
 
-			// Start the block to simulate AJAX requests
-			$panel.block({ message: null, overlayCSS: { background: '#fff url('+varmeta.assets_url+'/assets/images/ajax-loader.gif) no-repeat center', opacity: 0.6 } });
+            if ( ! $('.jigoshop_variation').size() )
+                return alert( varmeta.i18n.variations_required );
 
-			// Remove the variation from the posts array
-			$.post(varmeta.ajax_url, data, function(response) {
-				$panel.fadeOut('slow', function() {
-					$panel.remove();
-				});
-			});
+            // Cache variables
+            var $this = $(this);
+            $this.trigger( $this.prev().val() );
+        })
 
-		}
-		else {
+        .on({
+            remove_all: function(e) {
+                if( ! confirm( varmeta.i18n.remove_all ) )
+                    return false;
 
-			// Variation hasn't been saved so just remove the panel
-			$panel.fadeOut('slow', function() {
-				$panel.remove();
-			});
-		}
-	}
+                $('.jigoshop_variation').each( function() {
+                    remove_variation( $(this) );
+                });
+            },
+            set_all_regular_prices: function(e) {
+                value = prompt( varmeta.i18n.set_regular_price );
+                $(' input[name*="regular_price"] ').val( value );
+            },
+            set_all_sale_prices: function(e) {
+                value = prompt( varmeta.i18n.set_sale_price );
+                $(' input[name*="sale_price"] ').val( value );
+            },
+            set_all_stock: function(e) {
+                value = prompt( varmeta.i18n.set_stock );
+                $(' input[name*="stock"] ').val( value );
+            },
+            set_all_weight: function(e) {
+                value = prompt( varmeta.i18n.set_weight );
+                $(' input[name*="weight"] ').val( value );
+            },
+            set_all_width: function(e) {
+                value = prompt( varmeta.i18n.set_width );
+                $(' input[name*="width"] ').val( value );
+            },
+            set_all_length: function(e) {
+                value = prompt( varmeta.i18n.set_length );
+                $(' input[name*="length"] ').val( value );
+            },
+            set_all_height: function(e) {
+                value = prompt( varmeta.i18n.set_height );
+                $(' input[name*="height"] ').val( value );
+            }
+        });
+
+    });
+
+    function remove_variation( $panel ) {
+
+        // Set up the variables
+        var variation   = $panel.attr('rel');
+            data        = {
+                action: varmeta.actions.remove.action,
+                variation_id: variation,
+                security: varmeta.actions.remove.nonce
+            };
+
+        // If the variation already exists
+        if ( variation.indexOf('_new') < 0 ) {
+
+            // Start the block to simulate AJAX requests
+            $panel.block({ message: null, overlayCSS: { background: '#fff url('+varmeta.assets_url+'/assets/images/ajax-loader.gif) no-repeat center', opacity: 0.6 } });
+
+            // Remove the variation from the posts array
+            $.post(varmeta.ajax_url, data, function(response) {
+                $panel.fadeOut('slow', function() {
+                    $panel.remove();
+                });
+            });
+
+        }
+        else {
+
+            // Variation hasn't been saved so just remove the panel
+            $panel.fadeOut('slow', function() {
+                $panel.remove();
+            });
+        }
+    }
 
 })(window.jQuery);
