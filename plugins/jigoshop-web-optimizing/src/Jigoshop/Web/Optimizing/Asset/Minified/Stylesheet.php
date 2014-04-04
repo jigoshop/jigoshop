843f8665b8ea2c8f319b161437576a9b2ddc8ff8
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-04-04 16:19:56 +0200

    Integrate JWOF

---
+++ plugins/jigoshop-web-optimizing/src/Jigoshop/Web/Optimizing/Asset/Minified/Stylesheet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Jigoshop\Web\Optimizing\Asset\Minified;
+
+use Jigoshop\Web\Optimizing\Asset\Stylesheet as BaseStylesheet;
+
+/**
+ * Class representing stylesheet asset.
+ *
+ * @package Jigoshop\Web\Optimizing
+ */
+class Stylesheet extends BaseStylesheet
+{
+	protected function getAssetFilters()
+	{
+		return array('cssmin');
+	}
+}
