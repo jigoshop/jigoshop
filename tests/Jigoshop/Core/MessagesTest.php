b4d4eb642cf50a2c34c6c976e587d05eedecd95a
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-04-21 12:26:18 +0200

    Update Messages tests

--- tests/Jigoshop/Core/MessagesTest.php
+++ tests/Jigoshop/Core/MessagesTest.php
@@ -9,63 +9,69 @@ namespace Jigoshop\Core;
  */
 class MessagesTest extends \PHPUnit_Framework_TestCase
 {
-	/** @var Messages */
-	private $messages;
-
 	/** @var \Jigoshop\Core\Wordpress */
 	private $wordpress;
 
 	public function setUp()
 	{
 		$this->wordpress = $this->getMock('\\Jigoshop\\Core\\Wordpress');
-		$this->messages = new Messages($this->wordpress);
 	}
 
 	public function testAddNotice()
 	{
+		// Given
+		$messages = new Messages($this->wordpress);
+
 		// When
-		$this->messages->addNotice('test', false);
+		$messages->addNotice('test', false);
 
 		// Then
-		$notices = $this->messages->getNotices();
+		$notices = $messages->getNotices();
 		$this->assertCount(1, $notices);
 		$this->assertEquals('test', $notices[0]);
 	}
 
 	public function testAddWarning()
 	{
+		// Given
+		$messages = new Messages($this->wordpress);
+
 		// When
-		$this->messages->addWarning('test', false);
+		$messages->addWarning('test', false);
 
 		// Then
-		$warnings = $this->messages->getWarnings();
+		$warnings = $messages->getWarnings();
 		$this->assertCount(1, $warnings);
 		$this->assertEquals('test', $warnings[0]);
 	}
 
 	public function testAddError()
 	{
+		// Given
+		$messages = new Messages($this->wordpress);
+
 		// When
-		$this->messages->addError('test', false);
+		$messages->addError('test', false);
 
 		// Then
-		$errors = $this->messages->getErrors();
+		$errors = $messages->getErrors();
 		$this->assertCount(1, $errors);
 		$this->assertEquals('test', $errors[0]);
 	}
 
-	public function testPreserveMessages()
+	public function testPreservingMessages()
 	{
 		// Given
-		$this->messages->addError('test', false);
-		$this->messages->addError('preserved', true);
+		$messages = new Messages($this->wordpress);
+		$messages->addError('test', false);
+		$messages->addError('preserved', true);
 
 		// When
-		$this->messages->preserveMessages();
+		$messages->preserveMessages();
 
 		// Then
 		$errors = $_SESSION[Messages::ERRORS];
-		$this->assertCount(2, $this->messages->getErrors());
+		$this->assertCount(2, $messages->getErrors());
 		$this->assertCount(1, $errors);
 		$this->assertEquals('preserved', $errors[0]['message']);
 	}
