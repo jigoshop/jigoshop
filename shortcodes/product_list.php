fe92e110a4946dcc8c9a248cf69f67fbc48d9fb2
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-06-18 22:47:32 +0200

    Implement row visibility of the product_list shortcode

--- shortcodes/product_list.php
+++ shortcodes/product_list.php
@@ -1,32 +1,27 @@
 <?php
 
-function jigoshop_product_list($attributes) {
+function jigoshop_product_list($attributes)
+{
 	$options = Jigoshop_Base::get_options();
 
 	$attributes = shortcode_atts(array(
 		'number' => $options->get_option('jigoshop_catalog_per_page'),
-		'columns' => $options->get_option('jigoshop_catalog_columns'),
 		'order_by' => 'date',
 		'order' => 'desc',
 		'orientation' => 'rows',
 		'taxonomy' => 'product_cat',
 		'terms' => '',
+		'thumbnails' => 'show',
+		'sku' => 'hide',
 	), $attributes);
 
-	$query = new WP_Query(array(
+	$args = array(
 		'post_type' => 'product',
 		'post_status' => 'publish',
 		'ignore_sticky_posts' => 1,
 		'posts_per_page' => $attributes['number'],
 		'orderby' => $attributes['order_by'],
 		'order' => $attributes['order'],
-		'tax_query' => array(
-			array(
-				'taxonomy' => $attributes['taxonomy'],
-				'terms' => $attributes['terms'],
-				'field' => 'slug',
-			),
-		),
 		'meta_query' => array(
 			array(
 				'key' => 'visibility',
@@ -34,12 +29,52 @@ function jigoshop_product_list($attributes) {
 				'compare' => 'IN',
 			),
 		),
+	);
+
+	if(!empty($attributes['taxonomy']) && !empty($attributes['terms'])){
+		$args['tax_query'] = array(
+			array(
+				'taxonomy' => $attributes['taxonomy'],
+				'terms' => $attributes['terms'],
+				'field' => 'slug',
+			),
+		);
+	}
+
+	$query = new WP_Query($args);
+
+	remove_action('jigoshop_before_shop_loop_item_title', 'jigoshop_template_loop_product_thumbnail', 10);
+	if ($attributes['thumbnails'] === 'show') {
+		add_action('jigoshop_before_shop_loop_item', 'jigoshop_product_thumbnail', 10, 2);
+	}
+	if ($attributes['sku'] === 'show') {
+		add_action('jigoshop_after_shop_loop_item_title', 'jigoshop_product_sku', 9, 2);
+	}
+
+	$result = jigoshop_render_result('shortcode/product_list', array(
+		'orientation' => $attributes['orientation'],
+		'products' => $query->get_posts(),
+		'has_thumbnails' => $attributes['thumbnails'] === 'show'
 	));
 
-	return jigoshop_render_result('shortcode/product_list', array(
-		'orientation' => $attributes['orientation'],
-		'columns' => $attributes['columns'],
-		'products' => $query->get_posts(),
-	));
+	if ($attributes['sku'] === 'show') {
+		remove_action('jigoshop_after_shop_loop_item_title', 'jigoshop_product_sku', 9);
+	}
+	if ($attributes['thumbnails'] === 'show') {
+		remove_action('jigoshop_before_shop_loop_item', 'jigoshop_product_thumbnail', 10);
+	}
+	add_action('jigoshop_before_shop_loop_item_title', 'jigoshop_template_loop_product_thumbnail', 10, 2);
+
+	return $result;
 }
+
 add_shortcode('jigoshop_product_list', 'jigoshop_product_list');
+
+function jigoshop_product_sku($post, jigoshop_product $product)
+{
+	echo '<span class="sku">'.__('SKU', 'jigoshop').': '.$product->get_sku().'</span>';
+}
+function jigoshop_product_thumbnail($post, jigoshop_product $product)
+{
+	echo jigoshop_get_product_thumbnail('shop_thumbnail');
+}
--- templates/shortcode/product_list.php
+++ templates/shortcode/product_list.php
@@ -2,19 +2,24 @@
 /**
  * @var $orientation string CSS class for orientation of items in the list.
  * @var $products array List of products to display.
+ * @var $has_thumbnails boolean Whether we want to display thumbnails or not.
  */
 ?>
-<ul class="products <?= $orientation; ?>">
+<ul class="products <?= $orientation; ?> <?= $has_thumbnails ? 'thumbnails' : ''; ?>">
 	<?php foreach($products as $post): ?>
 	<li>
 		<?php $product = new jigoshop_product($post->ID); ?>
-		<?php do_action('jigoshop_before_shop_loop_item'); ?>
-		<a href="<?= $product->get_link(); ?>">
+		<div class="before-product">
+			<?php do_action('jigoshop_before_shop_loop_item', $post, $product); ?>
+		</div>
+		<a class="product" href="<?= $product->get_link(); ?>">
 			<?php do_action('jigoshop_before_shop_loop_item_title', $post, $product); ?>
 			<strong><?= $product->get_title(); ?></strong>
 			<?php do_action('jigoshop_after_shop_loop_item_title', $post, $product); ?>
 		</a>
-		<?php do_action('jigoshop_after_shop_loop_item', $post, $product); ?>
+		<div class="after-product">
+			<?php do_action('jigoshop_after_shop_loop_item', $post, $product); ?>
+		</div>
 	</li>
 	<?php endforeach; ?>
 </ul>
