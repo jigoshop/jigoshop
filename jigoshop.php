177ff0fb7afb327a21a0dbc51cc58566053510df
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-07-21 11:31:45 +0200

    Fix memory checking for some users

--- jigoshop.php
+++ jigoshop.php
@@ -20,7 +20,7 @@
  * Description:         Jigoshop, a WordPress eCommerce plugin that works.
  * Author:              Jigoshop
  * Author URI:          http://www.jigoshop.com
- * Version:             1.10.6
+ * Version:             1.10.7
  * Requires at least:   3.8
  * Tested up to:        3.9.1
  * Text Domain:         jigoshop
@@ -38,7 +38,7 @@
  */
 
 if (!defined('JIGOSHOP_VERSION')) {
-	define('JIGOSHOP_VERSION', '1.10.6');
+	define('JIGOSHOP_VERSION', '1.10.7');
 }
 if (!defined('JIGOSHOP_DB_VERSION')) {
 	define('JIGOSHOP_DB_VERSION', 1407060);
