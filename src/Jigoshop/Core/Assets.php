8162de75b7f6bad3d32711f8301d6e59ad0090d5
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-07-31 13:38:49 +0200

    Add products scripts only on product pages

--- src/Jigoshop/Core/Assets.php
+++ src/Jigoshop/Core/Assets.php
@@ -50,20 +50,17 @@ class Assets
 		$this->styles->add('jigoshop-admin', JIGOSHOP_URL.'/assets/css/admin.css');
 		$this->styles->add('jigoshop-vendors', JIGOSHOP_URL.'/assets/css/vendors.min.css');
 		$this->scripts->add('jigoshop-admin', JIGOSHOP_URL.'/assets/js/admin.js');
-		$this->scripts->add('jigoshop-admin-product', JIGOSHOP_URL.'/assets/js/admin/product.js');
 		$this->scripts->add('jigoshop-vendors', JIGOSHOP_URL.'/assets/js/vendors.min.js');
-//		$this->scripts->add('bootstrap', JIGOSHOP_URL.'/assets/js/bootstrap.min.js', array('jquery'));
 
-		// Insert Select2
-//		$this->styles->add('select2', JIGOSHOP_URL.'/assets/css/select2.css');
-//		$this->styles->add('select2', JIGOSHOP_URL.'/assets/css/select2-bootstrap.css', array('bootstrap'));
-//		$this->scripts->add('select2', JIGOSHOP_URL.'/assets/js/select2.min.js', array('jquery'));
+		if ($adminPage === 'product') {
+			$this->scripts->add('jigoshop-admin-product', JIGOSHOP_URL.'/assets/js/admin/product.js');
+		}
+//		$this->scripts->add('bootstrap', JIGOSHOP_URL.'/assets/js/bootstrap.min.js', array('jquery'));
 
 //		$this->styles->add('jigoshop_admin_styles', JIGOSHOP_URL.'/assets/css/admin.css');
 //		$this->styles->add('jquery-ui-jigoshop-styles', JIGOSHOP_URL.'/assets/css/jquery-ui-1.8.16.jigoshop.css');
 //		$this->styles->add('jigoshop-required', JIGOSHOP_URL.'/assets/css/required.css');
 
-//		$this->scripts->add('jigoshop-select2', JIGOSHOP_URL.'/assets/js/select2.min.js', array('jquery'));
 //		$this->scripts->add('jigoshop_blockui', JIGOSHOP_URL.'/assets/js/blockui.js', array('jquery'), array('version' => '2.4.6'));
 //		$this->scripts->add('jigoshop_backend', JIGOSHOP_URL.'/assets/js/jigoshop_backend.js', array('jquery'), array('version' => '1.0'));
 
