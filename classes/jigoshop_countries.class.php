7822ad060a2563b777f0b04ebd18712f6cee5ddd
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-09-23 11:33:33 +0200

    Remove leftover var_dump

--- classes/jigoshop_countries.class.php
+++ classes/jigoshop_countries.class.php
@@ -1012,7 +1012,6 @@ class jigoshop_countries extends Jigoshop_Base {
 		$echo = true,
 		$add_empty = false
 	){
-		var_dump($selected_country, $selected_state);
 		$output = '';
 		$countries = self::get_countries();
 
