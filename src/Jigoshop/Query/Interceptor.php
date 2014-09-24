02d5ebca9a51d4b17e1fc326cfa504d6478a4cf7
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-09-24 16:16:09 +0200

    Return proper request when entering shop list page

--- src/Jigoshop/Query/Interceptor.php
+++ src/Jigoshop/Query/Interceptor.php
@@ -3,24 +3,29 @@
 namespace Jigoshop\Query;
 
 use Jigoshop\Core\Options;
+use Jigoshop\Core\Pages;
 use Jigoshop\Core\Types;
+use Jigoshop\Entity\Product;
 use WPAL\Wordpress;
 
 class Interceptor
 {
 	private $intercepted = false;
+	/** @var Wordpress */
+	private $wp;
 	/** @var Options */
 	private $options;
 
 	public function __construct(Wordpress $wp, Options $options)
 	{
+		$this->wp = $wp;
 		$this->options = $options;
 		$wp->addFilter('request', array($this, 'intercept'));
 	}
 
 	public function intercept($request)
 	{
-		if ($this->intercepted) {
+		if ($this->intercepted || $this->wp->isAdmin()) {
 			return $request;
 		}
 
@@ -31,14 +36,34 @@ class Interceptor
 	private function parseRequest($request)
 	{
 		// TODO: Refactor preparing requests
-		if (isset($request['post_type']) && $request['post_type'] == Types::PRODUCT) {
+		if ($this->isProductList($request)) {
 			$options = $this->options->get('shopping');
-
-			$request['posts_per_page'] = $options['catalog_per_page'];
-			$request['orderby'] = $options['catalog_order_by'];
-			$request['order'] = $options['catalog_order'];
+			$request = array(
+				'post_type' => Types::PRODUCT,
+				'post_status' => 'publish',
+				'ignore_sticky_posts' => true,
+				'posts_per_page' => $options['catalog_per_page'],
+				'paged' => isset($request['paged']) ? $request['paged'] : 1,
+				'orderby' => $options['catalog_order_by'],
+				'order' => $options['catalog_order'],
+				'meta_query' => array(
+					array(
+						'key' => 'visibility',
+						'value' => array(Product::VISIBILITY_CATALOG, Product::VISIBILITY_PUBLIC),
+						'compare' => 'IN'
+					),
+				),
+			);
 		}
 
 		return $request;
+	}
+
+	private function isProductList($request)
+	{
+		return !isset($request['product']) && (
+			(isset($request['pagename']) && $request['pagename'] == Pages::SHOP) ||
+			(isset($request['post_type']) && $request['post_type'] == Types::PRODUCT)
+		);
 	}
 }
