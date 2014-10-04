47656a1edaa423a7027974a7ea625aa693b370d2
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-10-04 12:57:20 +0200

    Add cart totals to cart template

--- templates/shop/cart.php
+++ templates/shop/cart.php
@@ -60,15 +60,118 @@ use Jigoshop\Helper\Render;
 						</noscript>
 					</td>
 				</tr>
-				<?php if (false && $options->get('jigoshop_cart_shows_shop_button') == 'yes') : ?>
-					<tr>
-						<td colspan="6" class="actions">
-							<a href="<?php echo esc_url(jigoshop_cart::get_shop_url()); ?>" class="checkout-button button-alt" style="float:left;"><?php _e('&larr; Return to Shop', 'jigoshop'); ?></a>
-							<a href="<?php echo esc_url(jigoshop_cart::get_checkout_url()); ?>" class="checkout-button button-alt"><?php _e('Proceed to Checkout &rarr;', 'jigoshop'); ?></a>
-						</td>
-					</tr>
-				<?php endif; ?>
 			</tfoot>
 		</table>
+		<div id="cart-collaterals">
+			<?php do_action('cart-collaterals', $cart); ?>
+			<div id="cart-totals" class="panel panel-default pull-right">
+				<div class="panel-heading"><h2 class="panel-title"><?php _e('Cart Totals', 'jigoshop'); ?></h2></div>
+				<table class="table">
+					<tbody>
+<!--					<tr>-->
+<!--						<th scope="row">--><?php //_e('Subtotal', 'jigoshop'); ?><!--</th>-->
+<!--						<td>--><?php //// TODO: Show subtotal echo $cart->getTotal(); ?><!--</td>-->
+<!--					</tr>-->
+					<tr id="cart-total">
+						<th scope="row"><?php _e('Total', 'jigoshop'); ?></th>
+						<td><?php echo Product::formatPrice($cart->getTotal()); ?></td>
+					</tr>
+					</tbody>
+				</table>
+			<?php /*
+			// Hide totals if customer has set location and there are no methods going there
+			$available_methods = jigoshop_shipping::get_available_shipping_methods();
+
+			if ($available_methods || !jigoshop_customer::get_shipping_country() || !jigoshop_shipping::is_enabled()):
+				do_action('jigoshop_before_cart_totals');
+				?>
+				<h2><?php _e('Cart Totals', 'jigoshop'); ?></h2>
+				<div class="cart_totals_table">
+					<table cellspacing="0" cellpadding="0">
+						<tbody>
+						<tr>
+							<?php $price_label = jigoshop_cart::show_retail_price() ? __('Retail Price', 'jigoshop') : __('Subtotal', 'jigoshop'); ?>
+							<th class="cart-row-subtotal-title"><?php echo $price_label; ?></th>
+							<td class="cart-row-subtotal"><?php echo jigoshop_cart::get_cart_subtotal(true, false, true); ?></td>
+						</tr>
+						<?php if (jigoshop_cart::get_cart_shipping_total()): ?>
+							<tr>
+								<th class="cart-row-shipping-title"><?php _e('Shipping', 'jigoshop'); ?>
+									<small><?php echo _x('To: ', 'shipping destination', 'jigoshop').__(jigoshop_customer::get_shipping_country_or_state(), 'jigoshop'); ?></small>
+								</th>
+								<td class="cart-row-shipping"><?php echo jigoshop_cart::get_cart_shipping_total(true, true); ?>
+									<small><?php echo jigoshop_cart::get_cart_shipping_title(); ?></small>
+								</td>
+							</tr>
+						<?php endif; ?>
+						<?php if (jigoshop_cart::show_retail_price() && $options->get('jigoshop_prices_include_tax') == 'no'): ?>
+							<tr>
+								<th class="cart-row-subtotal-title"><?php _e('Subtotal', 'jigoshop'); ?></th>
+								<td class="cart-row-subtotal"><?php echo jigoshop_cart::get_cart_subtotal(true, true); ?></td>
+							</tr>
+						<?php elseif (jigoshop_cart::show_retail_price()): ?>
+							<tr>
+								<th class="cart-row-subtotal-title"><?php _e('Subtotal', 'jigoshop'); ?></th>
+								<?php
+								$price = jigoshop_cart::$cart_contents_total_ex_tax + jigoshop_cart::$shipping_total;
+								$price = jigoshop_price($price, array('ex_tax_label' => 1));
+								?>
+								<td class="cart-row-subtotal"><?php echo $price; ?></td>
+							</tr>
+						<?php endif; ?>
+						<?php if (jigoshop_cart::tax_after_coupon()): ?>
+							<tr class="discount">
+								<th class="cart-row-discount-title"><?php _e('Discount', 'jigoshop'); ?></th>
+								<td class="cart-row-discount">-<?php echo jigoshop_cart::get_total_discount(); ?></td>
+							</tr>
+						<?php endif; ?>
+						<?php if ($options->get('jigoshop_calc_taxes') == 'yes'):
+							foreach (jigoshop_cart::get_applied_tax_classes() as $tax_class):
+								if (jigoshop_cart::get_tax_for_display($tax_class)) : ?>
+									<tr data-tax="<?php echo $tax_class; ?>">
+										<th class="cart-row-tax-title"><?php echo jigoshop_cart::get_tax_for_display($tax_class) ?></th>
+										<td class="cart-row-tax"><?php echo jigoshop_cart::get_tax_amount($tax_class) ?></td>
+									</tr>
+								<?php
+								endif;
+							endforeach;
+						endif; ?>
+						<?php if (!jigoshop_cart::tax_after_coupon() && jigoshop_cart::get_total_discount()): ?>
+							<tr class="discount">
+								<th class="cart-row-discount-title"><?php _e('Discount', 'jigoshop'); ?></th>
+								<td class="cart-row-discount">-<?php echo jigoshop_cart::get_total_discount(); ?></td>
+							</tr>
+						<?php endif; ?>
+						<tr>
+							<th class="cart-row-total-title"><strong><?php _e('Total', 'jigoshop'); ?></strong></th>
+							<td class="cart-row-total"><strong><?php echo jigoshop_cart::get_total(); ?></strong></td>
+						</tr>
+						</tbody>
+					</table>
+				</div>
+				<?php
+				do_action('jigoshop_after_cart_totals');
+			else :
+				echo '<p>'.__(jigoshop_shipping::get_shipping_error_message(), 'jigoshop').'</p>';
+			endif;
+			?>
+		</div>
+		<?php
+		do_action('jigoshop_before_shipping_calculator');
+		jigoshop_shipping_calculator();
+		do_action('jigoshop_after_shipping_calculator');
+		//*/ ?>
+			</div>
+		</div>
+		<?php /* if (false && $options->get('jigoshop_cart_shows_shop_button') == 'yes') : ?>
+			<tr>
+				<td colspan="6" class="actions">
+					<a href="<?php echo esc_url(jigoshop_cart::get_shop_url()); ?>" class="checkout-button button-alt" style="float:left;"><?php _e('&larr; Return to Shop', 'jigoshop'); ?></a>
+					<a href="<?php echo esc_url(jigoshop_cart::get_checkout_url()); ?>" class="checkout-button button-alt"><?php _e('Proceed to Checkout &rarr;', 'jigoshop'); ?></a>
+				</td>
+			</tr>
+		<?php endif; //*/ ?>
+		<a href="<?php echo $shopUrl; ?>" class="btn btn-default pull-left"><?php _e('&larr; Return to shopping', 'jigoshop'); ?></a>
+		<button class="btn btn-primary pull-right"><?php _e('Proceed to checkout &rarr;', 'jigoshopp'); ?></button>
 	</form>
 <?php endif; ?>
