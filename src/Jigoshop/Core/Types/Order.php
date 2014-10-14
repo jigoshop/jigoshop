74352bb82a4b491669c82eed9aa821ef7e89e4e7
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-10-14 13:47:44 +0200

    Add support for filtering per order status

--- src/Jigoshop/Core/Types/Order.php
+++ src/Jigoshop/Core/Types/Order.php
@@ -4,6 +4,7 @@ namespace Jigoshop\Core\Types;
 
 use Jigoshop\Core\Options;
 use Jigoshop\Core\Types;
+use Jigoshop\Entity\Order\Status;
 use Jigoshop\Helper\Order as OrderHelper;
 use Jigoshop\Helper\Product;
 use Jigoshop\Helper\Styles;
@@ -31,23 +32,36 @@ class Order implements Post
 		$this->orderService = $orderService;
 		$this->taxService = $taxService;
 
+		$wp->addFilter('post_row_actions', array($this, 'displayTitle'));
+		$wp->addFilter('post_updated_messages', array($this, 'updateMessages'));
+		$wp->addFilter('request', array($this, 'request'));
+		$wp->addFilter(sprintf('bulk_actions-edit-%s', self::NAME), array($this, 'bulkActions'));
+		$wp->addFilter(sprintf('views_edit-%s', self::NAME), array($this, 'statusFilters'));
 		$wp->addFilter(sprintf('manage_edit-%s_columns', Types::ORDER), array($this, 'columns'));
 		$wp->addAction(sprintf('manage_%s_posts_custom_column', Types::ORDER), array($this, 'displayColumn'), 2);
+		$wp->addAction('init', array($this, 'registerOrderStatuses'));
 		$wp->addAction('admin_enqueue_scripts', function() use ($wp, $styles){
 			if ($wp->getPostType() == Order::NAME) {
 				$styles->add('jigoshop.admin.orders', JIGOSHOP_URL.'/assets/css/admin/orders.css');
 			}
 		});
-		// TODO: Introduce proper category filter
-//		$wp->addAction('restrict_manage_posts', array($this, 'categoryFilter'));
-//		$wp->addAction('restrict_manage_posts', array($this, 'typeFilter'));
 //		$that = $this;
 //		$wp->addAction('add_meta_boxes_'.self::NAME, function() use ($wp, $that){
 //			$wp->addMetaBox('jigoshop-product-data', __('Product Data', 'jigoshop'), array($that, 'box'), $that::NAME, 'normal', 'high');
 //			$wp->removeMetaBox('commentstatusdiv', null, 'normal');
 //		});
 
-		$wp->addFilter('post_row_actions', array($this, 'displayTitle'));
+	}
+
+	public function request($vars)
+	{
+		if ($this->wp->getPostType() === self::NAME) {
+			if (!isset($vars['post_status'])) {
+				$vars['post_status'] = array_keys(Status::getStatuses());
+			}
+		}
+
+		return $vars;
 	}
 
 	public function getName()
@@ -102,10 +116,24 @@ class Order implements Post
 			'shipping_address' => _x('Shipping address', 'order', 'jigoshop'),
 			'shipping_payment' => _x('Shipping &amp; Payment', 'order', 'jigoshop'),
 			'total' => _x('Total', 'order', 'jigoshop'),
-			'creation' => _x('Created at', 'product', 'jigoshop'),
 		);
 
 		return $columns;
+	}
+
+	public function registerOrderStatuses()
+	{
+		$statuses = Status::getStatuses();
+		foreach ($statuses as $status => $label) {
+			register_post_status($status, array(
+				'label' => _x('On hold', 'order-status', 'jigoshop'),
+				'public' => false,
+				'exclude_from_search' => false,
+				'show_in_admin_all_list' => true,
+				'show_in_admin_status_list' => true,
+				'label_count' => _n_noop($label.' <span class="count">(%s)</span>', $label.' <span class="count">(%s)</span>', 'jigoshop'),
+			));
+		}
 	}
 
 	public function displayColumn($column)
@@ -147,12 +175,64 @@ class Order implements Post
 		$post = $this->wp->getGlobalPost();
 		// TODO: Remember to save order title as "Order %d"
 
+		// Remove "Quick edit" as we won't use it.
+		unset($actions['inline hide-if-no-js']);
+
 		if ($post->post_type == self::NAME) {
 			$fullFormat = _x('Y/m/d g:i:s A', 'time', 'jigoshop');
 			$format = _x('Y/m/d', 'time', 'jigoshop');
+			// TODO: Exclude mysql2date to WPAL
 			echo '<time title="'.mysql2date($fullFormat, $post->post_date).'">'.apply_filters('post_date_column_time', mysql2date($format, $post->post_date), $post ).'</time>';
 		}
 
 		return $actions;
+	}
+
+	public function bulkActions($actions)
+	{
+		unset($actions['edit']);
+		return $actions;
+	}
+
+	function statusFilters($views)
+	{
+		$current = (isset($_GET['post_status']) && Status::exists($_GET['post_status'])) ? $_GET['post_status'] : '';
+		$statuses = Status::getStatuses();
+		$counts = $this->wp->wpCountPosts(self::NAME, 'readable');
+
+		$dates = isset($_GET['m']) ? '&amp;m='.$_GET['m'] : '';
+		foreach ($statuses as $status => $label) {
+			$count = isset($counts->$status) ? $counts->$status : 0;
+			$views[$status] = '<a class="'.$status.($current == $status ? ' current' : '').'" href="?post_type='.self::NAME.'&amp;post_status='.$status.$dates.'">'.$label.' <span class="count">('.$count.')</a>';
+		}
+
+		if (!empty($current)) {
+			$views['all'] = str_replace('current', '', $views['all']);
+		}
+
+		unset($views['publish']);
+
+		if (isset($views['trash'])) {
+			$trash = $views['trash'];
+			unset($views['draft']);
+			unset($views['trash']);
+			$views['trash'] = $trash;
+		}
+
+		return $views;
+	}
+
+	public function updateMessages($messages)
+	{
+		if ($this->wp->getPostType() === 'shop_order') {
+			$messages['post'][1] = __('Order updated.', 'jigoshop');
+			$messages['post'][4] = __('Order updated.', 'jigoshop');
+			$messages['post'][6] = __('Order published.', 'jigoshop');
+
+			$messages['post'][8] = __('Order submitted.', 'jigoshop');
+			$messages['post'][10] = __('Order draft updated.', 'jigoshop');
+		}
+
+		return $messages;
 	}
 }
--- src/Jigoshop/Entity/Order/Status.php
+++ src/Jigoshop/Entity/Order/Status.php
@@ -10,15 +10,50 @@ namespace Jigoshop\Entity\Order;
  */
 class Status
 {
-	const CREATED = 'new';
-	const PENDING = 'pending';
-	const ON_HOLD = 'on-hold';
-	const PROCESSING = 'processing';
-	const COMPLETED = 'completed';
-	const CANCELLED = 'cancelled';
-	const REFUNDED = 'refunded';
+	const CREATED = 'jigoshop-new';
+	const PENDING = 'jigoshop-pending';
+	const ON_HOLD = 'jigoshop-on-hold';
+	const PROCESSING = 'jigoshop-processing';
+	const COMPLETED = 'jigoshop-completed';
+	const CANCELLED = 'jigoshop-cancelled';
+	const REFUNDED = 'jigoshop-refunded';
 //			'failed' => __('Failed', 'jigoshop'), /* can be set from PayPal, not currently shown anywhere -JAP- */
 //			'denied' => __('Denied', 'jigoshop'), /* can be set from PayPal, not currently shown anywhere -JAP- */
 //			'expired' => __('Expired', 'jigoshop'), /* can be set from PayPal, not currently shown anywhere -JAP- */
 //			'voided' => __('Voided', 'jigoshop'), /* can be set from PayPal, not currently shown anywhere -JAP- */
+
+	private static $statuses;
+
+	/**
+	 * Checks if selected status exists.
+	 *
+	 * @param $status string Status name.
+	 * @return bool Does status exists?
+	 */
+	public static function exists($status)
+	{
+		$statuses = self::getStatuses();
+
+		return isset($statuses[$status]);
+	}
+
+	/**
+	 * @return array List of available order statuses.
+	 */
+	public static function getStatuses()
+	{
+		if (self::$statuses === null) {
+			self::$statuses = apply_filters('jigoshop\order\statuses', array(
+				Status::CREATED => __('New', 'jigoshop'),
+				Status::PENDING => __('Pending', 'jigoshop'),
+				Status::ON_HOLD => __('On-Hold', 'jigoshop'),
+				Status::PROCESSING => __('Processing', 'jigoshop'),
+				Status::COMPLETED => __('Completed', 'jigoshop'),
+				Status::CANCELLED => __('Cancelled', 'jigoshop'),
+				Status::REFUNDED => __('Refunded', 'jigoshop'),
+			));
+		}
+
+		return self::$statuses;
+	}
 }
