b224e7bfb9bdf6aa1c8d6ec6d63d88592dc6e08c
KrzysztofBorbis <krzysztof@borbis.pl>
2014-11-18 10:31:55 +0100

    wrapped code, used wp_ajax

--- admin/write-panels/email-data.php
+++ admin/write-panels/email-data.php
@@ -15,14 +15,21 @@ function jigoshop_email_data_box($post)
 		<script>
 			jQuery(document).ready(function($) {
 				$('#jigoshop_email_actions').change(function() {
-					var posting = $.post('post.php', {
-						jigoshop_email_get_variables: '1',
-						jigoshop_email_actions: $('#jigoshop_email_actions').val()
+					$.ajax({
+						type: "POST",
+						url: jigoshop_params.ajax_url,
+						data: {
+							'action':'update_variable_list',
+							'jigoshop_email_actions' : $('#jigoshop_email_actions').val()
+						},
+						success:function(data) {
+							$('#avalible_arguments').replaceWith(data);
+						},
+						error: function(errorThrown){
+							console.log(errorThrown);
+						}
 					});
-					// Put the results in a div
-					posting.done(function(data) {
-						$('#avalible_arguments').replaceWith    (data);
-					});
+
 				});
 			});
 		</script>
@@ -65,39 +72,10 @@ function jigoshop_email_variable_box($post)
 	?>
 	<div id="coupon_options" class="panel jigoshop_options_panel">
 		<div class="options_group">
-			<div id="avalible_arguments">
-				<table width="100%">
-					<?php
-					$i = 0;
-					$selected = (array)get_post_meta($post->ID, 'jigoshop_email_actions', true);
-					$registered_mails = jigoshop_emails::get_mail_list();
-					if (!empty($selected[0]) && !empty($registered_mails[$selected[0]])) {
-
-						$keys = array_keys($registered_mails[$selected[0]]['accepted_args']);
-
-						if (count($selected) > 1) {
-							foreach ($selected as $hook) {
-								$keys = array_intersect(array_keys($registered_mails[$hook]['accepted_args']), $keys);
-							}
-						}
-						asort ($keys);
-						if(empty($keys)){
-							_e('Selected actions have not common variables', 'jigoshop');
-						}
-						foreach ($keys as $key) : ?>
-							<?php if($i % 3 == 0): ?> <tr> <?php endif; ?>
-								<td width="33.33%"><strong>[<?php echo $key ?>] </strong> - <?php echo $registered_mails[$selected[0]]['accepted_args'][$key] ?> <br/>
-							<?php if($i % 3 == 2): ?> </tr> <?php endif; ?>
-							<?php $i++; ?>
-						<?php endforeach;
-					} else {
-						_e('Select email action to see avalible variables', 'jigoshop');
-					}?>
-				</table>
-			</div>
+			<?php update_variable_list($post); ?>
 		</div>
 	</div>
-<?php
+	<?php
 }
 
 add_action('jigoshop_process_shop_email_meta', 'jigoshop_process_shop_email_meta', 1, 2);
@@ -109,40 +87,38 @@ function jigoshop_process_shop_email_meta($post_id, $post)
 	jigoshop_emails::set_actions($post_id, isset($_POST['jigoshop_email_actions']) ? $_POST['jigoshop_email_actions'] : '');
 }
 
-add_action('init', function (){
-	if (isset($_POST['jigoshop_email_get_variables'])) {
-		?><div id="avalible_arguments">
-			<table width="100%">
-				<?php
-				$i = 0;
-				$selected = $_POST['jigoshop_email_actions'];
-				$registered_mails = jigoshop_emails::get_mail_list();
-				if (!empty($selected[0]) && !empty($registered_mails[$selected[0]])) {
+add_action('wp_ajax_update_variable_list',        'update_variable_list');
+add_action('wp_ajax_nopriv_update_variable_list', 'update_variable_list');
 
-					$keys = array_keys($registered_mails[$selected[0]]['accepted_args']);
-
-					if ($selected[1]) {
-						foreach ($selected as $hook) {
-							$keys = array_intersect(array_keys($registered_mails[$hook]['accepted_args']), $keys);
-						}
+function update_variable_list($post = ''){
+	?><div id="avalible_arguments">
+		<table width="100%">
+			<?php
+			$i = 0;
+			$selected = isset($_POST['jigoshop_email_actions']) ? $_POST['jigoshop_email_actions'] : (array)get_post_meta($post->ID, 'jigoshop_email_actions', true);
+			$registered_mails = jigoshop_emails::get_mail_list();
+			if (!empty($selected[0]) && !empty($registered_mails[$selected[0]])) {
+				$keys = array_keys($registered_mails[$selected[0]]['accepted_args']);
+				if ($selected[1]) {
+					foreach ($selected as $hook) {
+						$keys = array_intersect(array_keys($registered_mails[$hook]['accepted_args']), $keys);
 					}
-					asort($keys);
-					if(empty($keys)){
-						_e('Selected actions have not common variables', 'jigoshop');
-					}
-					foreach ($keys as $key) : ?>
-						<?php if($i % 3 == 0): ?> <tr> <?php endif; ?>
-							<td width="33.33%"><strong>[<?php echo $key ?>] </strong> - <?php echo $registered_mails[$selected[0]]['accepted_args'][$key] ?> <br/>
-						<?php if($i % 3 == 2): ?> </tr> <?php endif; ?>
-						<?php $i++; ?>
-					<?php endforeach;
-				} else {
-					_e('Select email action to see avalible variables', 'jigoshop');
-				}?>
-			</table>
-		</div><?php
-		exit;
-	}
-}, 11);
+				}
+				asort($keys);
+				if(empty($keys)){
+					_e('Selected actions have not common variables', 'jigoshop');
+				}
+				foreach ($keys as $key) : ?>
+					<?php if($i % 3 == 0): ?> <tr> <?php endif; ?>
+						<td width="33.33%"><strong>[<?php echo $key ?>] </strong> - <?php echo $registered_mails[$selected[0]]['accepted_args'][$key] ?> <br/>
+					<?php if($i % 3 == 2): ?> </tr> <?php endif; ?>
+					<?php $i++; ?>
+				<?php endforeach;
+			} else {
+				_e('Select email action to see avalible variables', 'jigoshop');
+			}?>
+		</table>
+	</div><?php
+}
 
 
