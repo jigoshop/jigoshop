afbdd66339f7e396edf4bf666ee11dd8b802e413
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2014-11-30 09:20:50 +0100

    Fix stock tab in product

--- templates/admin/product/box/stock.php
+++ templates/admin/product/box/stock.php
@@ -5,6 +5,7 @@ use Jigoshop\Entity\Product;
 /**
  * @var $product Product The product.
  */
+$stock = $product instanceof Product\Purchasable ? $product->getStock() : new Product\Attributes\StockStatus();
 ?>
 <fieldset>
 	<?php
@@ -12,34 +13,34 @@ use Jigoshop\Entity\Product;
 		'name' => 'product[stock][manage]',
 		'id' => 'stock-manage',
 		'label' => __('Manage stock?', 'jigoshop'),
-		'checked' => $product->getStock()->getManage(),
+		'checked' => $stock->getManage(),
 	));
 	Forms::select(array(
 		'name' => 'product[stock][status]',
 		'id' => 'stock-status',
 		'label' => __('Status', 'jigoshop'),
-		'value' => $product->getStock()->getStatus(),
+		'value' => $stock->getStatus(),
 		'options' => array(
 			Product\Attributes\StockStatus::IN_STOCK => __('In stock', 'jigoshop'),
 			Product\Attributes\StockStatus::OUT_STOCK => __('Out of stock', 'jigoshop'),
 		),
-		'classes' => array($product->getStock()->getManage() ? 'not-active' : ''),
+		'classes' => array($stock->getManage() ? 'not-active' : ''),
 	));
 	?>
 </fieldset>
-<fieldset class="stock-status" style="<?php !$product->getStock()->getManage() and print 'display: none;'; ?>">
+<fieldset class="stock-status" style="<?php !$stock->getManage() and print 'display: none;'; ?>">
 	<?php
 	Forms::text(array(
 		'name' => 'product[stock][stock]',
 		'label' => __('Items in stock', 'jigoshop'),
-		'value' => $product->getStock()->getStock(),
+		'value' => $stock->getStock(),
 	));
 	?>
 	<?php
 	Forms::select(array(
 		'name' => 'product[stock][allow_backorders]',
 		'label' => __('Allow backorders?', 'jigoshop'),
-		'value' => $product->getStock()->getAllowBackorders(),
+		'value' => $stock->getAllowBackorders(),
 		'options' => array(
 			'no' => __('Do not allow', 'jigoshop'),
 			'notify' => __('Allow, but notify customer', 'jigoshop'),
