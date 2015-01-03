25bb49d81b057a6e776a008c6c6db9cd96d381d7
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2015-01-03 22:17:41 +0100

    Bump version to beta7

--- src/Jigoshop/Core.php
+++ src/Jigoshop/Core.php
@@ -12,7 +12,7 @@ use WPAL\Wordpress;
 
 class Core
 {
-	const VERSION = '2.0-beta6';
+	const VERSION = '2.0-beta7';
 	const WIDGET_CACHE = 'jigoshop_widget_cache';
 
 	/** @var \Jigoshop\Core\Options */
