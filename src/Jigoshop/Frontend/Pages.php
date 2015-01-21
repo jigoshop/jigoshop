4e30394b1b3048e503c47b99641a8be1bd0d7124
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2015-01-21 14:54:13 +0100

    Fix notices about undefined index action

--- src/Jigoshop/Frontend/Pages.php
+++ src/Jigoshop/Frontend/Pages.php
@@ -116,10 +116,19 @@ class Pages
 		if (!isset(self::$cache[self::CART])) {
 			$page = self::$options->getPageId(self::CART);
 			self::$cache[self::CART] = $page !== false && is_page($page);
-			self::$cache[self::CART] |= self::isAjax() && strpos($_REQUEST['action'], 'cart') !== false;
+			self::$cache[self::CART] |= self::isAjax() && isset($_REQUEST['action']) && strpos($_REQUEST['action'], 'cart') !== false;
 		}
 
 		return self::$cache[self::CART];
+	}
+
+	public static function isAjax()
+	{
+		if (defined('DOING_AJAX') && isset($_REQUEST['action']) && strpos($_REQUEST['action'], 'jigoshop') !== false) {
+			return true;
+		}
+
+		return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
 	}
 
 	/**
@@ -133,10 +142,133 @@ class Pages
 		if (!isset(self::$cache[self::CHECKOUT])) {
 			$page = self::$options->getPageId(self::CHECKOUT);
 			self::$cache[self::CHECKOUT] = $page !== false && is_page($page);
-			self::$cache[self::CHECKOUT] |= self::isAjax() && strpos($_REQUEST['action'], 'checkout') !== false;
+			self::$cache[self::CHECKOUT] |= self::isAjax() && isset($_REQUEST['action']) && strpos($_REQUEST['action'], 'checkout') !== false;
 		}
 
 		return self::$cache[self::CHECKOUT];
+	}
+
+	/**
+	 * Evaluates to true only on the Single Product Page
+	 *
+*@return bool
+	 * @since 2.0
+	 */
+	public static function isProduct()
+	{
+		if (!isset(self::$cache[self::PRODUCT])) {
+			self::$cache[self::PRODUCT] = is_singular(array(Types::PRODUCT));
+		}
+
+		return self::$cache[self::PRODUCT];
+	}
+
+	/**
+	 * Evaluates to true only on the Category Pages
+
+	 *
+*@return bool
+	 * @since 2.0
+	 */
+	public static function isProductCategory()
+	{
+		if (!isset(self::$cache[self::PRODUCT_CATEGORY])) {
+			self::$cache[self::PRODUCT_CATEGORY] = is_tax(Types::PRODUCT_CATEGORY);
+		}
+
+		return self::$cache[self::PRODUCT_CATEGORY];
+	}
+
+	/**
+	 * Evaluates to true only on Shop, Product Category, and Product Tag pages
+
+	 *
+*@return bool
+	 * @since 2.0
+	 */
+	public static function isProductList()
+	{
+		if (!isset(self::$cache[self::PRODUCT_LIST])) {
+			$page = self::$options->getPageId(self::SHOP);
+			self::$cache[self::PRODUCT_LIST] = is_post_type_archive(Types::PRODUCT) ||
+				($page !== false && is_page($page)) ||
+				self::isProductCategory() ||
+				self::isProductTag();
+		}
+
+		return self::$cache[self::PRODUCT_LIST];
+	}
+
+	/**
+	 * Evaluates to true only on the Tag Pages
+
+	 *
+*@return bool
+	 * @since 2.0
+	 */
+	public static function isProductTag()
+	{
+		if (!isset(self::$cache[self::PRODUCT_TAG])) {
+			self::$cache[self::PRODUCT_TAG] = is_tax(Types::PRODUCT_TAG);
+		}
+
+		return self::$cache[self::PRODUCT_TAG];
+	}
+
+	/**
+	 * Evaluates to true only on the main Account or any sub-account pages
+
+	 *
+*@return bool
+	 * @since 2.0
+	 */
+	public static function isAccount()
+	{
+		if (!isset(self::$cache[self::ACCOUNT])) {
+			$page = self::$options->getPageId(self::ACCOUNT);
+			self::$cache[self::ACCOUNT] = $page !== false && is_page($page);
+		}
+
+		return self::$cache[self::ACCOUNT];
+	}
+
+	/**
+	 * @param $page string Page to check.
+	 * @return boolean Is current page selected one?
+	 * @since 2.0
+	 */
+	public static function isAdminPage($page)
+	{
+		if (!isset(self::$cache[$page])) {
+			self::$cache[$page] = self::getAdminPage() == $page;
+		}
+
+		return self::$cache[$page];
+	}
+
+	/**
+	 * @return string|bool Currently displayed admin page slug or false.
+	 */
+	public static function getAdminPage()
+	{
+		global $current_screen;
+
+		if ($current_screen === null) {
+			return false;
+		}
+
+		//		if (in_array($currentScreen->post_type, array(Types::PRODUCT, Types::ORDER, Types::COUPON), true)) {
+		if (in_array($current_screen->post_type, array(Types::PRODUCT, Types::ORDER), true)) {
+			return $current_screen->post_type;
+		}
+		if (strpos($current_screen->id, 'jigoshop') !== false) {
+			return $current_screen->id;
+		}
+		if (strpos($current_screen->base, 'jigoshop_page') !== false) {
+			return $current_screen->base;
+		}
+
+		return false;
 	}
 
 	/**
@@ -150,7 +282,7 @@ class Pages
 		if (!isset(self::$cache[self::THANK_YOU])) {
 			$page = self::$options->getPageId(self::THANK_YOU);
 			self::$cache[self::THANK_YOU] = $page !== false && is_page($page);
-			self::$cache[self::THANK_YOU] |= self::isAjax() && strpos($_REQUEST['action'], 'thank_you') !== false;
+			self::$cache[self::THANK_YOU] |= self::isAjax() && isset($_REQUEST['action']) && strpos($_REQUEST['action'], 'thank_you') !== false;
 		}
 
 		return self::$cache[self::THANK_YOU];
@@ -170,86 +302,6 @@ class Pages
 		}
 
 		return self::$cache['checkout-pay'];
-	}
-
-	/**
-	 * Evaluates to true only on the Single Product Page
-	 *
-	 * @return bool
-	 * @since 2.0
-	 */
-	public static function isProduct()
-	{
-		if (!isset(self::$cache[self::PRODUCT])) {
-			self::$cache[self::PRODUCT] =  is_singular(array(Types::PRODUCT));
-		}
-
-		return self::$cache[self::PRODUCT];
-	}
-
-	/**
-	 * Evaluates to true only on the Category Pages
-	 *
-	 * @return bool
-	 * @since 2.0
-	 */
-	public static function isProductCategory()
-	{
-		if (!isset(self::$cache[self::PRODUCT_CATEGORY])) {
-			self::$cache[self::PRODUCT_CATEGORY] =  is_tax(Types::PRODUCT_CATEGORY);
-		}
-
-		return self::$cache[self::PRODUCT_CATEGORY];
-	}
-
-	/**
-	 * Evaluates to true only on Shop, Product Category, and Product Tag pages
-	 *
-	 * @return bool
-	 * @since 2.0
-	 */
-	public static function isProductList()
-	{
-		if (!isset(self::$cache[self::PRODUCT_LIST])) {
-			$page = self::$options->getPageId(self::SHOP);
-			self::$cache[self::PRODUCT_LIST] = is_post_type_archive(Types::PRODUCT) ||
-				($page !== false && is_page($page)) ||
-				self::isProductCategory() ||
-				self::isProductTag();
-		}
-
-		return self::$cache[self::PRODUCT_LIST];
-	}
-
-	/**
-	 * Evaluates to true only on the Tag Pages
-	 *
-	 * @return bool
-	 * @since 2.0
-	 */
-	public static function isProductTag()
-	{
-		if (!isset(self::$cache[self::PRODUCT_TAG])) {
-			self::$cache[self::PRODUCT_TAG] =  is_tax(Types::PRODUCT_TAG);
-		}
-
-		return self::$cache[self::PRODUCT_TAG];
-	}
-
-	/**
-	 * Evaluates to true only on the main Account or any sub-account pages
-	 *
-	 * @return bool
-	 * @since 2.0
-	 */
-	public static function isAccount()
-	{
-		if (!isset(self::$cache[self::ACCOUNT])) {
-			$page = self::$options->getPageId(self::ACCOUNT);
-			self::$cache[self::ACCOUNT] = $page !== false && is_page($page);
-		}
-
-		return self::$cache[self::ACCOUNT];
 	}
 
 	/**
@@ -301,61 +353,6 @@ class Pages
 	}
 
 	/**
-	 * Evaluates to true only on the Order Tracking page
-	 *
-	 * @return bool
-	 * @since 2.0
-	 */
-	public static function isOrderTracker()
-	{
-		if (!isset(self::$cache[self::ORDER_TRACKING])) {
-			$page = self::$options->getPageId(self::ORDER_TRACKING);
-			self::$cache[self::ORDER_TRACKING] = $page !== false && is_page($page);
-		}
-
-		return self::$cache[self::ORDER_TRACKING];
-	}
-
-	/**
-	 * @param $page string Page to check.
-	 * @return boolean Is current page selected one?
-	 * @since 2.0
-	 */
-	public static function isAdminPage($page)
-	{
-		if (!isset(self::$cache[$page])) {
-			self::$cache[$page] = self::getAdminPage() == $page;
-		}
-
-		return self::$cache[$page];
-	}
-
-	/**
-	 * @return string|bool Currently displayed admin page slug or false.
-	 */
-	public static function getAdminPage()
-	{
-		global $current_screen;
-
-		if ($current_screen === null) {
-			return false;
-		}
-
-		//		if (in_array($currentScreen->post_type, array(Types::PRODUCT, Types::ORDER, Types::COUPON), true)) {
-		if (in_array($current_screen->post_type, array(Types::PRODUCT, Types::ORDER), true)) {
-			return $current_screen->post_type;
-		}
-		if (strpos($current_screen->id, 'jigoshop') !== false) {
-			return $current_screen->id;
-		}
-		if (strpos($current_screen->base, 'jigoshop_page') !== false) {
-			return $current_screen->base;
-		}
-
-		return false;
-	}
-
-	/**
 	 * Evaluates to true for all Jigoshop pages
 	 *
 	 * @return bool
@@ -377,12 +374,19 @@ class Pages
 		return self::isProductList() || self::isProduct() || self::isProductCategory() || self::isProductTag();
 	}
 
-	public static function isAjax()
+	/**
+	 * Evaluates to true only on the Order Tracking page
+	 *
+	 * @return bool
+	 * @since 2.0
+	 */
+	public static function isOrderTracker()
 	{
-		if (defined('DOING_AJAX') && strpos($_REQUEST['action'], 'jigoshop') !== false) {
-			return true;
+		if (!isset(self::$cache[self::ORDER_TRACKING])) {
+			$page = self::$options->getPageId(self::ORDER_TRACKING);
+			self::$cache[self::ORDER_TRACKING] = $page !== false && is_page($page);
 		}
 
-		return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
+		return self::$cache[self::ORDER_TRACKING];
 	}
 }
