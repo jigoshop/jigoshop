cad72eb7a6d67475cee04ddd80d6018ceb7f158e
Tomasz Dyda <nofacexx@gmail.com>
2016-06-07 16:02:35 +0200

    ++ accept terms

---
+++ templates/admin/migration-terms.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('ABSPATH'))
+{
+	exit;
+}
+
+?>
+<div class="wrap jigoshop jigoshop-migration-ask-wrap">
+	<h2><?php _e('Jigoshop Migration Information', 'jigoshop'); ?></h2>
+	<form action="" method="POST">
+		We need information about your plugins' compatibility (for instance plugins' versions) to check, if they're ready to be updated to their respective Jigoshop 2 versions.<br />
+		<button name="migration_terms_accept" style="margin-top: 20px;">I agree, check the compatibility</button>
+	</form>
+</div>
